51432058f0d5ef56b268f7438527f5c6cbc8afd5
HalfMoonFatty <allofotherstuff@gmail.com>
2016-08-23 21:35:32 -0700

    Update 283. Move Zeroes.py

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -1,9 +1,9 @@
 '''
 Problem:
 
-Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+    Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
-For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+    For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
 
 Note:
     You must do this in-place without making a copy of the array.
@@ -12,7 +12,6 @@ Note:
 
 class Solution(object):
     def moveZeroes(self, nums):
-
         zInd = 0
         for i in range(len(nums)):
             if nums[i] != 0:
