19670248884b74dc05e4adaf923a6b6ccb308c60
HalfMoonFatty <allofotherstuff@gmail.com>
2016-08-23 22:12:59 -0700

    Update 142. Linked List Cycle II.py

--- 142. Linked List Cycle II.py
+++ 142. Linked List Cycle II.py
@@ -1,12 +1,11 @@
+'''
 Problem:
 
 	Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
-Note:
-    Do not modify the linked list.
+Note: Do not modify the linked list.
 
-Follow up:
-    Can you solve it without using extra space?
+Follow up: Can you solve it without using extra space?
 
 '''
 
