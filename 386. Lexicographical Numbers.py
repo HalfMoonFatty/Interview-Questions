c057a4408924f94605c49ba0e7a10c4cc4e92f67
HalfMoonFatty <allofotherstuff@gmail.com>
2016-08-24 14:49:17 -0700

    Create 386. Lexicographical Numbers.py

---
+++ 386. Lexicographical Numbers.py
@@ -0,0 +1,26 @@
+'''
+Problem:
+
+    Given an integer n, return 1 - n in lexicographical order.
+
+    For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
+
+    Please optimize your algorithm to use less time and space. The input size may be as large as 5,000,000.
+
+'''
+
+class Solution(object):
+    def lexicalOrder(self, n):
+
+        def dfs(res,result):
+            if res <= n:
+                result.append(res)
+            if res*10 <= n: # optimization
+                for i in range(10):
+                    dfs(res*10+i, result)
+
+
+        result = []
+        for i in range(1,10):
+            dfs(i,result)
+        return result
