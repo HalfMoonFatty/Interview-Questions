ac592f4c01a855c8cd190601ea97062a869953e4
HalfMoonFatty <allofotherstuff@gmail.com>
2016-08-25 23:11:49 -0700

    Create 010. Regular Expression Matching.py

---
+++ 010. Regular Expression Matching.py
@@ -0,0 +1,44 @@
+'''
+Problem:
+
+Implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "a*") → true
+isMatch("aa", ".*") → true
+isMatch("ab", ".*") → true
+isMatch("aab", "c*a*b") → true
+
+'''
+
+class Solution(object):
+    def isMatch(self, s, p):
+        m = len(s)
+        n = len(p)
+        dp = [[False]*(n+1) for _ in range(m+1)]
+
+        dp[0][0] = True
+        for i in range(1,m+1):
+            dp[i][0] = False
+        # edges 2. p[0.., j - 3, j - 2, j - 1] matches empty iff p[j - 1] is '*' and p[0..j - 3] matches empty
+        for j in range(1,n+1):
+            dp[0][j] = j > 1 and p[j - 1] == '*' and dp[0][j-2]
+
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if p[j-1] != '*':
+                    dp[i][j] = dp[i-1][j-1] and (p[j-1] == s[i-1] or p[j-1] == '.')
+                elif p[j-1] == '*': # else
+                    dp[i][j] = dp[i][j-2] or (p[j-2] == s[i-1] or p[j-2] == '.') and dp[i-1][j]
+        return dp[-1][-1]
