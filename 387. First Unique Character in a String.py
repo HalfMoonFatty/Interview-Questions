fdfafa000f8bd89cb3d71d90e75324f872f7323c
HalfMoonFatty <allofotherstuff@gmail.com>
2016-08-29 00:22:17 -0700

    Update 387. First Unique Character in a String.py

--- 387. First Unique Character in a String.py
+++ 387. First Unique Character in a String.py
@@ -21,19 +21,18 @@ Solution:
 
 class Solution(object):
     def firstUniqChar(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        ind = [0]*26
+
+        count = [0]*26
         for i in range(len(s)):
-            ind[ord(s[i])-ord('a')] += 1
+            count[ord(s[i])-ord('a')] += 1
             
         for i in range(len(s)):
-            if ind[ord(s[i])-ord('a')] == 1:
+            if count[ord(s[i])-ord('a')] == 1:
                 return i
                 
         return -1
+
+
 
 
 class Solution(object):
@@ -41,14 +40,11 @@ class Solution(object):
 
         if not s: return -1
         
-        mp = {}
-        for c in s:
-            mp[c] = []
-        
-        ind = 0
+        count = [0]*26
+        index = 0
         for i in range(len(s)):
-            mp[s[i]].append(i)
-            while ind < len(s) and len(mp[s[ind]]) > 1:
-                ind += 1
-
-        return -1 if ind == len(s) else ans
+            count[ord(s[i])-ord('a')] += 1
+            while index < len(s) and count[ord(s[index])-ord('a')] > 1:
+                index += 1
+                
+        return -1 if index == len(s) else index
