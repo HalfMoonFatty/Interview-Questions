0c6276289442b4e6e350d9230bbc91b5f9467cce
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-01 17:02:51 -0700

    Update shortestDistance.py

--- shortestDistance.py
+++ shortestDistance.py
@@ -53,3 +53,64 @@ class Solution(object):
 s = Solution()
 grid = [[1,0,2,0,1],[0,0,0,0,0],[0,0,1,0,0]]
 print s.shortestDistance(grid)
+
+
+
+
+
+# buggy
+from collections import deque
+import sys
+
+class Solution(object):
+    def shortestDistance(self, grid):
+        def canExplore(nx, ny):
+            return nx >= 0 and nx < m and ny >= 0 and ny < n and grid[nx][ny] == 0
+
+        def bfs_dist(i,j):
+            xdir = [0,1,0,-1]
+            ydir = [1,0,-1,0]
+            visited = [[False for yy in range(n)] for xx in range(m)]   # shadow var don't use i and j
+
+            q = deque()
+            q.append([i,j])
+            level = 1
+           
+            while len(q):
+                cur = q.popleft()
+                x,y = cur[0],cur[1]
+
+                for d in range(4):
+                    nx = x + xdir[d]
+                    ny = y + ydir[d]
+                    if canExplore(nx, ny) and not visited[nx][ny]:
+                        distance[nx][ny] = distance[x][y] +1
+                        reach[x][y] =  reach[x][y] + 1
+                        visited[nx][ny] = True
+                        q.append([nx,ny])
+            return
+
+
+        m,n = len(grid),len(grid[0])
+        reach = [[0 for j in range(n)] for i in range(m)]
+        distance = [[0 for j in range(n)] for i in range(m)]
+        numBuilding = 0
+       
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:   # for each building
+                    numBuilding += 1
+                    bfs_dist(i,j)
+
+
+        shortest = sys.maxint
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0 and reach[i][j] == numBuilding:
+                    shortest = min(shortest, distance[i][j])
+        return shortest if shortest < sys.maxint else -1
+
+
+s = Solution()
+grid = [[1,0,2,0,1],[0,0,0,0,0],[0,0,1,0,0]]
+print s.shortestDistance(grid)
