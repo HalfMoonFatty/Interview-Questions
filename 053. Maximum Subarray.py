7c9ed7a1007e7429c9d34463163ce963064c6314
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-04 23:20:17 -0700

    Create 053. Maximum Subarray.py

---
+++ 053. Maximum Subarray.py
@@ -0,0 +1,61 @@
+'''
+Problem:
+    Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+    For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+    the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+
+'''
+
+'''
+Solution 1: 
+	Reset sum to 0 when sum < 0
+	Time: O(n)
+	Space: O(1)
+'''
+class Solution(object):
+    def maxSubArray(self, nums):
+    
+        maxsum = 0
+        sum = 0
+    
+        for i in range(len(nums)):
+            sum += nums[i]
+            maxsum = max(maxsum,sum)
+            if sum < 0:
+                sum = 0    # Reset sum
+        return maxsum
+        
+        
+
+'''
+NOTE: 
+If the array is all negative numbers, what is the correct behavior? Consider this simple array {-3, -10, -5}. 
+You could make a good argument that the maximum sum is either: 
+(A) -3 (if you assume the subsequence can’t be empty) 
+(B) 0 (the subsequence has length 0) or 
+(C) MINIMUM_INT (essentially the error case). 
+We went with option B (max sum = 0), but there’s no “correct” answer. This is a great thing to discuss with your interviewer to show how careful you are.
+'''
+
+import sys
+class Solution(object):
+    def maxSubArray(self, nums):
+
+        maxsum,cursum = 0,0
+        maxNeg = -sys.maxint-1
+        allNeg = True
+
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                allNeg = False
+            else:
+                maxNeg = max(maxNeg,nums[i])
+                
+            cursum += nums[i]
+            maxsum = max(maxsum,cursum)
+            
+            if cursum < 0:
+                cursum = 0
+
+        return maxNeg if allNeg else maxsum
