e8c9cad349e4ecfa7cb8505dd41e6f25a4b6a2c4
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-08 18:31:31 -0700

    Update LintCode Coins in a Line III.py

--- LintCode Coins in a Line III.py
+++ LintCode Coins in a Line III.py
@@ -1,7 +1,10 @@
 '''
 Problem:
 
+There are n coins with different value in a line. Two players take turns to take one coins from left side or right side until there are no more coins left. 
+The player who take the coins with the most value wins.
 
+Could you please decide the first player will win or lose?
 '''
 
 # Solution:
@@ -9,11 +12,11 @@ state:
 	f[x][y] 现在还第x到第y的硬币,现在先手取硬币的人最后最多取硬币价值 
 
 function:
-	f[x][y] = max(min(f[x+2][y], f[x+1][y-1])+a[x] ) , (min(f[x][y-2], f[x+1][y-1])+a[y] )
+	f[x][y] = max(min(f[x+2][y], f[x+1][y-1])+a[x]) , (min(f[x][y-2], f[x+1][y-1])+a[y])
 
 intialize:
 	f[x][x] = a[x]
 	f[x][x+1] = max(a[x],a[x+1])
 	
-Answer:
+answer:
 	f[0][n] > sum(a)/2
