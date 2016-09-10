f9853df4327215b7ff884b8f9a2146eb822bfe94
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-09 23:54:55 -0700

    Update 188. Best Time to Buy and Sell Stock IV.py

--- 188. Best Time to Buy and Sell Stock IV.py
+++ 188. Best Time to Buy and Sell Stock IV.py
@@ -25,7 +25,7 @@ class Solution(object):
         if k > len(prices)/2:
             profit = 0
             for i in range(1,len(prices)):
-                profit += max(0,prices[i] - prices[i - 1])
+                profit += max(0,prices[i] - prices[i-1])
             return profit
 
         # DP part:
