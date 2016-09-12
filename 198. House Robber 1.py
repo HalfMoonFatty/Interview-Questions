14cbe3b7f9c32aa6188283ec036c8bdac7167395
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-12 10:43:57 -0700

    Update 198. House Robber 1.py

--- 198. House Robber 1.py
+++ 198. House Robber 1.py
@@ -20,9 +20,9 @@ class Solution(object):
     def rob(self, nums):
 
         size = len(nums)
-        dp = [0] * (size +1)
+        dp = [0] * (size+1)
         if size:
             dp[1] = nums[0]
-        for i in range(2, size +1):
-            dp[i] = max(dp[i -1], dp[i -2] + nums[i -1])
+        for i in range(2, size+1):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
         return dp[size]
