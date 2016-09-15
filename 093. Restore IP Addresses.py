bdf67cbf7fef7e07cb5865ccc30b009ea9be242f
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-14 22:03:31 -0700

    Create 093. Restore IP Addresses.py

---
+++ 093. Restore IP Addresses.py
@@ -0,0 +1,38 @@
+'''
+Problem:
+
+Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+For example:
+Given "25525511135",
+return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+'''
+
+
+class Solution(object):
+    def restoreIpAddresses(self, s):
+        """
+            :type s: str
+            :rtype: List[str]
+            """
+        def isValid(s):
+            if (len(s) == 1 and 0 <= int(s) <10) or (len(s) == 2 and 9 < int(s) < 100) or (len(s) == 3 and 99 < int(s) < 256):
+                return True
+            else:
+                return False
+
+        def getIpAddr(count,s,start,res,result):
+            # base
+            if count == 3 and isValid(s[start:]):
+                result.append(res+s[start:])
+                return
+            elif count < 3: # note
+                for i in range(1,4):
+                    if isValid(s[start:start+i]):
+                        getIpAddr(count+1, s, start+i, res+s[start:start+i]+".", result)
+                return
+
+
+        result = []
+        getIpAddr(0,s,0,"",result)
+        return result
