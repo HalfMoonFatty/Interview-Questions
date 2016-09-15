0473ee6e16bf66a61813edabdb4d83a535c03780
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-14 22:42:12 -0700

    Update 291. Word Pattern II.py

--- 291. Word Pattern II.py
+++ 291. Word Pattern II.py
@@ -5,9 +5,9 @@ Problem:
     Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty substring in str.
 
 Examples:
-     pattern = "abab", str = "redblueredblue" should return true.
-     pattern = "aaaa", str = "asdasdasdasd" should return true.
-     pattern = "aabb", str = "xyzabcxzyabc" should return false.
+    pattern = "abab", str = "redblueredblue" should return true.
+    pattern = "aaaa", str = "asdasdasdasd" should return true.
+    pattern = "aabb", str = "xyzabcxzyabc" should return false.
 
 Notes:
      You may assume both pattern and str contains only lowercase letters.
@@ -24,42 +24,29 @@ class Solution(object):
             """
         def isMatch(pattern, pstart, str, strStart, mp, st):
             # base case
-            if pstart == len(pattern) and strStart == len(str):
-                return True
-            if pstart == len(pattern) or strStart == len(str):
-                return False
-
-            # get current pattern character
-            p = pattern[pstart]
+            if pstart == len(pattern) and strStart == len(str): return True
+            if pstart == len(pattern) or strStart == len(str): return False
 
             # case 1: the pattern character exists
+            p = pattern[pstart]
             if mp.has_key(p):
                 word = mp[p]
-                # then check if we can use it to match str[i...i+s.length()]
-                if not str.startswith(word, strStart):
-                    return False
+                if not str.startswith(word, strStart): return False
                 return isMatch(pattern, pstart+1, str, strStart+len(word), mp, st)
-
-
+                
             # case 2: the pattern character does not exist in the map
             for k in range(strStart, len(str)):
                 newStr = str[strStart:k+1]
-                # if the newStr already exist in the set,
-                # and we can't find the pattern - string pair in the map (case1)
-                # we should also ignore this string here and append more character to the string
-                if newStr in st:
-                    continue
+                if newStr in st: continue    # ignore
                 # create new key in map and add the new string in the map
-                mp.setdefault(p, newStr)
+                mp[p] = newStr
                 st.add(newStr)
-                # continue to match the rest
                 if isMatch(pattern, pstart+1, str, strStart+len(newStr), mp, st):
                     return True
-                # backtracking
                 else:
-                    del mp[p]           # delete the entry in the map
-                    st.remove(newStr)   # delete the element in the set
-            # we've tried our best but still no luck
+                    del mp[p]           
+                    st.remove(newStr)   
+
             return False
 
         st = set()
