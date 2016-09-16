b68a297ab84d95075c64d28ae5208e5865348109
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-15 23:50:52 -0700

    Create 113. Path Sum II.py

---
+++ 113. Path Sum II.py
@@ -0,0 +1,53 @@
+'''
+Problem:
+
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+For example: Given the below binary tree and sum = 22,
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \    / \
+7    2  5   1
+return
+[
+[5,4,11,2],
+[5,8,4,5]
+]
+'''
+
+
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+            :type root: TreeNode
+            :type sum: int
+            :rtype: List[List[int]]
+            """
+
+        def findPath(root, sum, path, result):
+           # base case
+            if not root:
+                return
+
+            # reach leaf node
+            elif not root.left and not root.right:
+                sum -= root.val
+                if sum == 0: # valid path, add to the results
+                    path.append(root.val)
+                    result.append(path[:])
+                else:
+                    return # not valid path
+
+            else:
+                sum -= root.val
+                path.append(root.val)
+                findPath(root.left,sum,path[:],result)
+                findPath(root.right,sum,path[:],result)
+
+
+        result = []
+        findPath(root,sum,[],result)
+        return result
