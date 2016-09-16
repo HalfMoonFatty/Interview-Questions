4fcbc9fb55b3d7b4411e4012d4a753b4f53db7a0
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-16 11:00:07 -0700

    Create 107. Binary Tree Level Order Traversal II.py

---
+++ 107. Binary Tree Level Order Traversal II.py
@@ -0,0 +1,45 @@
+'''
+Problem:
+
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+      3
+     / \
+    9   20
+       /  \
+      15   7
+
+     return its bottom-up level order traversal as:
+[
+[15,7],
+[9,20],
+[3]
+]
+'''
+
+
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+            :type root: TreeNode
+            :rtype: List[List[int]]
+            """
+        def helper(root,level,result):
+
+            if not root:
+                return
+
+            if level == len(result):
+                result.insert(0,[])
+
+            result[len(result)-level-1].append(root.val)    # note
+            helper(root.left,level+1,result)
+            helper(root.right,level+1,result)
+            return
+
+        result = []
+        helper(root,0,result)
+        return result
