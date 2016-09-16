f5b28dbb788a3da05fc26c6b842de6087fe2b27e
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-16 11:04:04 -0700

    Create 314. Binary Tree Vertical Order Traversal.py

---
+++ 314. Binary Tree Vertical Order Traversal.py
@@ -0,0 +1,84 @@
+'''
+Problem:
+
+    Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
+    If two nodes are in the same row and column, the order should be from left to right.
+
+Examples:
+    Given binary tree [3,9,20,null,null,15,7],
+         3
+        / \
+       9  20
+     /  \
+    15   7
+    return its vertical order traversal as:
+    [
+    [9],
+    [3,15],
+    [20],
+    [7]
+    ]
+
+    Given binary tree [3,9,20,4,5,2,7],
+        _3_
+       /   \
+      9    20
+     / \   / \
+    4   5 2   7
+    return its vertical order traversal as:
+    [
+    [4],
+    [9],
+    [3,5,2],
+    [20],
+    [7]
+    ]
+
+'''
+
+'''
+Solution:
+
+    import collections
+    d = {2:3, 1:89, 4:5, 3:0}
+    od = collections.OrderedDict(sorted(d.items()))
+    print od
+    >> OrderedDict([(1, 89), (2, 3), (3, 0), (4, 5)])
+
+
+    And I used:
+    for k in sorted(mp):
+         ret.append(mp[k])
+
+    - mp: key is columnID; value is node.val
+    - tuples in the queue: (node, columnID); need to store the node in the queue because we need reference to the left and right child and also the value of the node
+'''
+
+from collections import deque
+class Solution(object):
+    def verticalOrder(self, root):
+        """
+            :type root: TreeNode
+            :rtype: List[List[int]]
+            """
+        mp = {}
+        q = deque()
+        q.append((root, 0))
+        while len(q) > 0:
+            elem = q.popleft()
+            if elem[0]:
+                node = elem[0]
+                key = elem[1]
+                if mp.has_key(key):
+                    mp[key].append(node.val)
+                else:
+                    mp[key] = [node.val]
+                q.append((node.left, key-1))
+                q.append((node.right, key+1))
+
+
+        ret = []
+        # note: need to sort by column
+        for k in sorted(mp):
+            ret.append(mp[k])
+        return ret
