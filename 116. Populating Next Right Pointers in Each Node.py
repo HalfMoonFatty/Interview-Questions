33d968e479f76dfe6d1ecd5289a0e3d38af8d279
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-16 11:42:11 -0700

    Update 116. Populating Next Right Pointers in Each Node.py

--- 116. Populating Next Right Pointers in Each Node.py
+++ 116. Populating Next Right Pointers in Each Node.py
@@ -37,20 +37,13 @@ After calling your function, the tree should look like:
 '''
 Solution 1: Iterative
 
-这个解法利用了题中重要条件：it is a perfect binary tree。所以只要没有了prev.left就说明没有“新的一行”了，也就是terminate condition.
+这个解法利用了题中重要条件：it is a perfect binary tree。所以只要没有了curr.left就说明没有“新的一行”了，也就是terminate condition.
 
                      1 -> NULL
                     /  \
-     cure = prev = 2 -> 3 -> NULL
+      itr = curr = 2 -> 3 -> NULL
                   / \  / \
                  4->5->6->7 -> NULL
-    curr = 2
-    curr.left.next = curr.right => connect 4->5
-    curr.right.next = curr.next.left => connect 5->6
-    curr = curr.next  => cure = 3
-    curr.left.next = curr.right => connect 6->7
-    curr.right.next = curr.next.left => connect 7->None
-
 '''
 
 class Solution(object):
@@ -60,14 +53,14 @@ class Solution(object):
             :rtype: nothing
             """
         if not root: return None
-        prev = root
-        prev.next = None
+        curr = root
+        curr.next = None
 
-        while prev and prev.left:
-            curr = prev
-            while curr:
-                curr.left.next = curr.right
-                if curr.next: curr.right.next = curr.next.left
-                curr = curr.next
-            prev = prev.left
+        while curr and curr.left:
+            itr = curr
+            while itr:
+                itr.left.next = itr.right
+                if itr.next: itr.right.next = itr.next.left
+                itr = itr.next
+            curr = curr.left
         return
