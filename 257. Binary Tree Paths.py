f143d4664030e2ff48308a80750e05054a4b7100
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-15 21:55:42 -0700

    Create 257. Binary Tree Paths.py

---
+++ 257. Binary Tree Paths.py
@@ -0,0 +1,39 @@
+'''
+Problem:
+
+Given a binary tree, return all root-to-leaf paths.
+For example, given the following binary tree:
+
+   1
+ /   \
+2     3
+ \
+  5
+All root-to-leaf paths are: ["1->2->5", "1->3"]
+'''
+
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {string[]}
+
+    def binaryTreePaths(self, root):
+
+        def helper(root, path, result):
+            if not root:
+                return
+
+            # leaf node
+            if not root.left and not root.right:
+                path += str(root.val)
+                result.append(path)
+                return
+
+            helper(root.left,path+str(root.val)+"->",result)
+            helper(root.right,path+str(root.val)+"->",result)
+
+
+        result = []
+        if not root: return result
+        helper(root,"",result)
+        return result
