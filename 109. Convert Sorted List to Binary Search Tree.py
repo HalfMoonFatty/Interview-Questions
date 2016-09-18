0b095abaa3b6ee92f473ee4f05def70c4564645a
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-17 22:09:56 -0700

    Create 109. Convert Sorted List to Binary Search Tree.py

---
+++ 109. Convert Sorted List to Binary Search Tree.py
@@ -0,0 +1,38 @@
+'''
+Problem:
+
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+'''
+
+
+class Solution(object):
+    def sortedListToBST(self, head):
+        """
+            :type head: ListNode
+            :rtype: TreeNode
+            """
+
+        start = head
+        # Base case:
+        if not start: return None
+        if not start.next: return TreeNode(start.val)    
+
+        # use fast and slow poiters to find the mid point
+        slow, fast = start, start
+        prev = ListNode(-10000)     # dummy node
+        prev.next = start
+        while fast and fast.next:
+            fast = fast.next.next   
+            slow = slow.next        # slow reaches mid
+            prev = prev.next
+
+        # start->...->prev-> mid(slow) ->start2->...->None
+        # Break the list into 2 lists
+        prev.next = None
+        start2 = slow.next
+
+        root = TreeNode(slow.val)    
+        root.left = self.sortedListToBST(start)
+        root.right = self.sortedListToBST(start2)
+        
+    return root
