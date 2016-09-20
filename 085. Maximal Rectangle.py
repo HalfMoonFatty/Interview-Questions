d68d2cfc125e97fcdbb216e76c1d6deda162d8fe
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-20 12:25:49 -0700

    Create 085. Maximal Rectangle.py

---
+++ 085. Maximal Rectangle.py
@@ -0,0 +1,44 @@
+'''
+Problem:
+
+Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+
+For example, given the following matrix:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+Return 6.
+
+'''
+
+
+
+class Solution(object):
+    def maximalRectangle(self, matrix):
+
+        if not matrix: return 0
+        m,n = len(matrix),len(matrix[0])
+
+        height = [0]*(n+1)
+        maxArea = 0
+
+        for i in range(m):
+            index = []
+            for j in range(n+1):  # NOTE n+1
+                if j < n:
+                    if matrix[i][j] == '1': height[j] += 1
+                    else: height[j] = 0  # reset height back to 0
+
+                # calculate maxArea
+                while len(index)>0 and height[index[-1]] > height[j]:
+                    h = height[index.pop()]    
+                    if len(index) > 0: startInd = index[-1] 
+                    else: startInd = -1
+                    maxArea = max(maxArea,h*(j-startInd-1))
+                    
+                index.append(j)
+                
+        return maxArea
+        
