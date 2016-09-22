bdece01b3f61600efa073a3f70697569cb86dee1
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-21 22:17:47 -0700

    Create 381. Insert Delete GetRandom O(1) - Duplicates allowed.py

---
+++ 381. Insert Delete GetRandom O(1) - Duplicates allowed.py
@@ -0,0 +1,91 @@
+'''
+Problem:
+
+Design a data structure that supports all following operations in average O(1) time.
+
+Note: Duplicate elements are allowed.
+insert(val): Inserts an item val to the collection.
+remove(val): Removes an item val from the collection if present.
+getRandom: Returns a random element from current collection of elements. The probability of each element being returned is linearly related to the number of same value the collection contains.
+Example:
+
+// Init an empty collection.
+RandomizedCollection collection = new RandomizedCollection();
+
+// Inserts 1 to the collection. Returns true as the collection did not contain 1.
+collection.insert(1);
+
+// Inserts another 1 to the collection. Returns false as the collection contained 1. Collection now contains [1,1].
+collection.insert(1);
+
+// Inserts 2 to the collection, returns true. Collection now contains [1,1,2].
+collection.insert(2);
+
+// getRandom should return 1 with the probability 2/3, and returns 2 with the probability 1/3.
+collection.getRandom();
+
+// Removes 1 from the collection, returns true. Collection now contains [1,2].
+collection.remove(1);
+
+// getRandom should return 1 and 2 both equally likely.
+collection.getRandom();
+
+'''
+
+
+
+
+
+import random
+class RandomizedCollection(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.arr = []
+        self.index_map = collections.defaultdict(set)
+        
+
+
+    def insert(self, val):
+        """
+        Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        self.arr.append(val)
+        self.index_map[val].add(len(self.arr)-1)
+        return len(self.index_map) == 1
+        
+        
+
+    def remove(self, val):
+        """
+        Removes a value from the collection. Returns true if the collection contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+
+        if not self.index_map[val]: return False
+        
+        # swap lastVal with delete Val
+        ind, lastVal = self.index_map[val].pop(), self.arr[-1]
+        self.arr[ind] = lastVal
+            
+        # update index_map
+        if self.index_map[lastVal]:
+            self.index_map[lastVal].add(ind)
+            self.index_map[lastVal].discard(len(self.arr)-1)
+                    
+        self.arr.pop()
+        return True
+        
+        
+
+    def getRandom(self):
+        """
+        Get a random element from the collection.
+        :rtype: int
+        """
+        return random.choice(self.arr)
