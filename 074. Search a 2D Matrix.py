3e5f0ff594617eaf55594c145be314a7d394ffb0
HalfMoonFatty <allofotherstuff@gmail.com>
2016-09-29 11:45:15 -0700

    Update 074. Search a 2D Matrix.py

--- 074. Search a 2D Matrix.py
+++ 074. Search a 2D Matrix.py
@@ -47,24 +47,13 @@ class Solution(object):
 
 
 
-'''
-Solution 2 - 1: Iterative Implementation 
-    Same as Search a 2D matrix II; The tricky part is to start from the left up corner.
-    - if the target is == matrix[i][j]: return "True" directly;
-    - elif target > matrix[i][j]: we need larger values, so seach the next row: i += 1
-    - else (target < matrix[i][j]): we need smaller values, so search the smaller column: j -= 1
-    row range [0,len(matrix)-1]
-    column range [len(matrix[0])-1, 0]
-'''
 
+# Solution 2 - 1: Iterative Implementation; The tricky part is to start from the left up corner.
 
 class Solution(object):
+    
     def searchMatrix(self, matrix, target):
-        """
-            :type matrix: List[List[int]]
-            :type target: int
-            :rtype: bool
-            """
+
         i = 0
         j = len(matrix[0])-1
        
@@ -80,21 +69,12 @@ class Solution(object):
 
 
 
-'''
-Solution 2 - 2: Recursive Implementation
-   
-    Don't know why cannot pass Leetcode oj large test cases with Runtime error.
-    Could becasue of python maximum recusion stack depth.
-'''
-   
 
- class Solution(object):
+# Solution 2 - 2: Recursive Implementation
+
+class Solution(object):
+    
     def searchMatrix(self, matrix, target):
-        """
-            :type matrix: List[List[int]]
-            :type target: int
-            :rtype: bool
-            """
        
         def helper(matrix,r,c,target):
             if r < 0 or c < 0 or r > len(matrix)-1 or c > len(matrix[0])-1:
