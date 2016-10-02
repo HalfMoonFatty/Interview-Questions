0779afca11818ffd81ae58a021ace3f8b0033a80
HalfMoonFatty <allofotherstuff@gmail.com>
2016-10-01 20:05:33 -0700

    Create 050. Pow(x, n).py

---
+++ 050. Pow(x, n).py
@@ -0,0 +1,24 @@
+'''
+Problem:
+
+    Implement pow(x, n).
+'''
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+
+        if n == 0: return 1
+        
+        if n < 0:    # note
+            return 1/self.myPow(x,-n)
+        
+        r = self.myPow(x,n/2)
+        if n%2:
+            return x*r*r
+        else:
+            return r*r
