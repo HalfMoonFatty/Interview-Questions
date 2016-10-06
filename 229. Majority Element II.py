75f3b09ffad1253ce793b43427b2a8e951d84068
HalfMoonFatty <allofotherstuff@gmail.com>
2016-10-05 17:52:15 -0700

    Update 229. Majority Element II.py

--- 229. Majority Element II.py
+++ 229. Majority Element II.py
@@ -11,43 +11,46 @@ The algorithm should run in linear time and in O(1) space.
 
 class Solution(object):
     def majorityElement(self, nums):
-
+        """
+            :type nums: List[int]
+            :rtype: List[int]
+            """
         if not nums or len(nums)<3:
             return list(set(nums))
 
         res = []
         n1 = None
         n2 = None
-        c1 = 0
-        c2 = 0
+        count1 = 0
+        count2 = 0
 
         for n in nums:
-            if c1>0 and c2>0:
+            if count1>0 and count2>0:
                 if n1 == n:
-                    c1 += 1
+                    count1 += 1
                 if n2 == n:
-                    c2 += 1
+                    count2 += 1
                 else:
-                    c1 -= 1
-                    c2 -= 1
+                    count1 -= 1
+                    count2 -= 1
 
-            elif c1>0:
+            elif count1>0:
                 if n1 == n:
-                    c1 += 1
+                    count1 += 1
                 else:
                     n2 = n
-                    c2 += 1
+                    count2 += 1
 
-            elif c2>0:
+            elif count2>0:
                 if n2 == n:
-                    c2 += 1
+                    count2 += 1
                 else:
                     n1 = n
-                    c1 += 1
+                    count1 += 1
 
             else:
                 n1 = n
-                c1 += 1
+                count1 += 1
 
 
 
