21425b1e7c55a2ae81785c7483b2898b423f9f25
HalfMoonFatty <allofotherstuff@gmail.com>
2016-10-05 22:28:41 -0700

    Create 028. Implement strStr().py

---
+++ 028. Implement strStr().py
@@ -0,0 +1,32 @@
+'''
+Problem:
+
+    Implement strStr().
+    Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+'''
+
+'''
+Solution: No need to use "KMP" algorithm. Brute Force with 2 pointers is already good enough.
+
+'''
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+
+        if not needle:
+            return 0
+            
+        m = len(haystack)
+        n = len(needle)
+        
+        for i in range(m-n+1):
+            j = 0
+            while j<n:
+                if haystack[i+j] != needle[j]:
+                    break
+                j +=1
+
+            if j == n:
+                return i
+        return -1
