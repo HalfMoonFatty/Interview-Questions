6d1ab3c95c4f5cb9688ef373470454058e835912
HalfMoonFatty <allofotherstuff@gmail.com>
2016-10-11 13:28:14 -0700

    Create 86. Partition List.py

---
+++ 86. Partition List.py
@@ -0,0 +1,53 @@
+'''
+Problem:
+
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+For example,
+     Given 1->4->3->2->5->2 and x = 3,
+     return 1->2->2->4->3->5.
+'''
+
+
+'''
+Solution:
+
+Time: O(n)
+Space: O(n)
+
+'''
+
+class Solution(object):
+    def partition(self, head, x):
+        """
+            :type head: ListNode
+            :type x: int
+            :rtype: ListNode
+            """
+        if not head:
+            return None
+
+        ltcur = ltlist = ListNode(0)    # dummy node for val < x
+        gtcur = gtlist = ListNode(0)    # dummy node for val >= x
+
+        # iterate the ll and partion into 2 lists
+        while head:
+            if head.val < x:
+                ltcur.next = head
+                ltcur = ltcur.next
+            else:
+                gtcur.next = head
+                gtcur = gtcur.next
+            head = head.next
+
+        lthead = ltlist.next
+        gthead = gtlist.next
+
+        # concatenate 2 lists, need to check if the list heads are None
+        if ltcur: ltcur.next = gthead
+        if gtcur: gtcur.next = None
+        return lthead if lthead else gthead
+
+        
