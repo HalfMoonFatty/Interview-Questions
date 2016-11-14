c3302db156f61f17b8bb5a5086dd5f963fac0053
HalfMoonFatty <allofotherstuff@gmail.com>
2016-11-13 23:48:23 -0800

    Create 202. Happy Number.py

---
+++ 202. Happy Number.py
@@ -0,0 +1,35 @@
+'''
+Problem:
+
+    Write an algorithm to determine if a number is "happy".
+
+    A happy number is a number defined by the following process:
+        Starting with any positive integer,
+        replace the number by the sum of the squares of its digits,
+        and repeat the process until the number equals 1 (where it will stay),
+        or it loops endlessly in a cycle which does not include 1.
+        Those numbers for which this process ends in 1 are happy numbers.
+
+    Example: 19 is a happy number
+    1^2 + 9^2 = 82
+    8^2 + 2^2 = 68
+    6^2 + 8^2 = 100
+    1^2 + 0^2 + 0^2 = 1
+
+'''
+
+
+
+class Solution:
+
+    def isHappy(self, n):
+        stop = {1}
+        while n not in stop:
+            stop.add(n)
+            sum = 0
+            while n:
+                dig = n%10
+                sum += dig*dig
+                n /= 10
+            n = sum
+        return n == 1
