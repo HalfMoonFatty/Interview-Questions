40a55e8681137e55d2963609c6268056599e0f6c
HalfMoonFatty <allofotherstuff@gmail.com>
2016-11-16 10:29:05 -0800

    Create 246. Strobogrammatic Number.py

---
+++ 246. Strobogrammatic Number.py
@@ -0,0 +1,40 @@
+'''
+Problem:
+
+A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+Write a function to determine if a number is strobogrammatic. The number is represented as a string.
+
+For example, the numbers "69", "88", and "818" are all strobogrammatic.
+'''
+
+
+'''
+Solution:
+
+Time: O(n)
+Space: O(1) (constant)
+'''
+
+class Solution(object):
+    def isStrobogrammatic(self, num):
+        """
+            :type num: str
+            :rtype: bool
+            """
+
+        mp = {}
+        mp.setdefault('6','9')
+        mp.setdefault('9','6')
+        mp.setdefault('0','0')
+        mp.setdefault('1','1')
+        mp.setdefault('8','8')
+
+        i,j = 0,len(num)-1
+        while i <= j:
+            if num[i] not in mp or num[j] not in mp:
+                return False
+            elif mp[num[i]] != num[j] or mp[num[j]] != num[i]:
+                return False
+            i += 1
+            j -= 1
+        return True
