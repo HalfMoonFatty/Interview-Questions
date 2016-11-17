867e6bd3d620d93914303fb1e72954a3fd45e547
HalfMoonFatty <allofotherstuff@gmail.com>
2016-11-16 21:41:50 -0800

    Create 345. Reverse Vowels of a String.py

---
+++ 345. Reverse Vowels of a String.py
@@ -0,0 +1,38 @@
+'''
+Problem:
+
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+Given s = "hello", return "holle".
+
+Example 2:
+Given s = "leetcode", return "leotcede".
+
+Company：
+    Google (phone)
+'''
+
+
+
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+            :type s: str
+            :rtype: str
+            """
+        def isVowel(char):
+            return char in ['a','e','i','o','u', 'A','E','I','O','U']
+
+        l = list(s)
+        start, end = 0, len(s) - 1
+        while start < end:
+            while not isVowel(l[start]) and start < end:
+                start += 1
+            while not isVowel(l[end]) and end > start:
+                end -= 1
+
+            l[start],l[end] = l[end],l[start]
+            start += 1
+            end -= 1
+        return ''.join(l)
