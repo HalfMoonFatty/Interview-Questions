69ae751e6cb551a34c9985b5fae7c4a6fd3d7d94
HalfMoonFatty <allofotherstuff@gmail.com>
2016-11-17 10:32:02 -0800

    Create !!! 352. Data Stream as Disjoint Intervals.py

---
+++ !!! 352. Data Stream as Disjoint Intervals.py
@@ -0,0 +1,6 @@
+
+
+https://discuss.leetcode.com/topic/47029/share-my-python-solution-using-heap
+https://discuss.leetcode.com/topic/61907/no-treeset-pure-arraylist
+https://discuss.leetcode.com/topic/47084/java-fast-log-n-solution-186ms-without-using-the-treemap-but-a-customized-bst/2
+https://discuss.leetcode.com/topic/46904/very-concise-c-solution
