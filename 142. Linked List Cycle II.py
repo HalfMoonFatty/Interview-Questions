45cbae600466063a834934ca35cc52a6d4545215
HalfMoonFatty <allofotherstuff@gmail.com>
2016-11-30 20:43:53 -0800

    Update 142. Linked List Cycle II.py

--- 142. Linked List Cycle II.py
+++ 142. Linked List Cycle II.py
@@ -1,7 +1,7 @@
 '''
 Problem:
 
-	Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
 Note: Do not modify the linked list.
 
