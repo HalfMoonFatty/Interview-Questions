fc080c82844d6d47ac063296d084eec5e38e5119
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-05 11:55:20 -0800

    Update 173. Binary Search Tree Iterator.py

--- 173. Binary Search Tree Iterator.py
+++ 173. Binary Search Tree Iterator.py
@@ -11,25 +11,31 @@ Note: next() and hasNext() should run in average O(1) time and uses O(h) memory,
 # Time  complexity: hasNext() in O(1) time; next() is O(h) time.
 
 
-class BSTIterator(object):
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class BSTIterator:
+
     def __init__(self, root):
         self.stack = []
-        self.helper(root)
-
-    def helper(self, root):
-        if not root:
-            return
-        self.stack.append(root)
-        if root.left:
-            self.helper(root.left)
-        return
-
+        self.pushLeft(root)
+            
+    def pushLeft(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
 
     def hasNext(self):
-        return len(self.stack) > 0
-
+        return self.stack
 
     def next(self):
-        ret = self.stack.pop()
-        self.helper(ret.right)
-        return ret.val
+        top = self.stack.pop()
+        self.pushLeft(top.right)
+        return top.val
+    
+
+
