8e5d813d6dd7e1979172c65393f5870baa916b81
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-15 22:01:50 -0800

    Update 372. Super Pow.py

--- 372. Super Pow.py
+++ 372. Super Pow.py
@@ -1,7 +1,8 @@
 '''
 Problem:
 
-Your task is to calculate ab mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
+Your task is to calculate ab mod 1337 where a is a positive integer 
+and b is an extremely large positive integer given in the form of an array.
 
     Example1:
     a = 2
