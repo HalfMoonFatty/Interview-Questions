e81aae10decbf3336f082450e1c499059a6c4fd6
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-15 18:04:28 -0800

    Update 361. Bomb Enemy.py

--- 361. Bomb Enemy.py
+++ 361. Bomb Enemy.py
@@ -1,8 +1,10 @@
 '''
 Problem:
 
-Given a 2D grid, each cell is either a wall 'W', an enemy 'E' or empty '0' (the number zero), return the maximum enemies you can kill using one bomb.
-The bomb kills all the enemies in the same row and column from the planted point until it hits the wall since the wall is too strong to be destroyed.
+Given a 2D grid, each cell is either a wall 'W', an enemy 'E' or empty '0' (the number zero), 
+return the maximum enemies you can kill using one bomb.
+The bomb kills all the enemies in the same row and column from the planted point 
+until it hits the wall since the wall is too strong to be destroyed.
 Note that you can only put the bomb at an empty cell.
 
 Example:
@@ -44,7 +46,7 @@ class Solution(object):
             for j in range(n):
                 if j == 0 or grid[i][j-1] == "W":
                     rowCache = 0
-                    for k in range(j,n):
+                    for k in range(j,n):    # note
                         if grid[i][k] == "W":
                             break
                         if grid[i][k] == "E":
@@ -53,7 +55,7 @@ class Solution(object):
 
                 if i == 0 or grid[i-1][j] == "W":
                     colCache[j] = 0
-                    for k in range(i,m):
+                    for k in range(i,m):   # note
                         if grid[k][j] == "W":
                             break
                         if grid[k][j] == "E":
