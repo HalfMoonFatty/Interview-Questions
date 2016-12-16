64c0a4adf2d1a971450b7dc7b09cd9a6552e0809
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-15 21:24:11 -0800

    Update 368. Largest Divisible Subset.py

--- 368. Largest Divisible Subset.py
+++ 368. Largest Divisible Subset.py
@@ -3,19 +3,15 @@ Problem:
 
 Given a set of distinct positive integers, find the largest subset such that 
 every pair (Si, Sj) of elements in this subset satisfies: Si % Sj = 0 or Sj % Si = 0.
-
 If there are multiple solutions, return any subset is fine.
 
-Example 1:
-nums: [1,2,3]
+Example 1: nums: [1,2,3]
 Result: [1,2] (of course, [1,3] will also be ok)
 
-
-Example 2:
-nums: [1,2,4,8]
+Example 2: nums: [1,2,4,8]
 Result: [1,2,4,8]
-
 '''
+
 
 '''
 Solution: 本题的key point 就是在如何记住 optimal solution 都包括哪些elements.
@@ -25,17 +21,17 @@ then try nums[j] == 3, 9%3 == 0, now dp[i] = dp[j]+1
    
 
 Note:
-1. Use prev to remember the trace of list of elements that forms the optimal result; 
-   Use maxIndex to remember the back-tracing starting point.
+    1. Use prev to remember the trace of list of elements that forms the optimal result; 
+       Use maxIndex to remember the back-tracing starting point.
 
-2. Only when nums[i]%nums[j] == 0 and "dp[j]+1>dp[i]", update prev[i].
-    normally, we do the following to update dp[i]:
+    2. Only when nums[i]%nums[j] == 0 and "dp[j]+1>dp[i]", update prev[i].
+       normally, we do the following to update dp[i]:
     
-    if nums[i]%nums[j] == 0:
-        dp[i] = max(dp[i],dp[j]+1)
+        if nums[i]%nums[j] == 0:
+            dp[i] = max(dp[i],dp[j]+1)
    
-    but here, we also need to update prev[i] to tracing the list of subset elements, 
-    so only when dp[j]+1>dp[i], we can update dp[i]. So we need to use:
+       but here, we also need to update prev[i] to tracing the list of subset elements, 
+       so only when dp[j]+1>dp[i], we can update dp[i]. So we need to use:
 '''
 
 class Solution(object):
