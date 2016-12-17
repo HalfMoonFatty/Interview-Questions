bb9e047c0898990bee9b247949a94bafcbffd036
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-17 13:29:25 -0800

    Update 398. Random Pick Index.py

--- 398. Random Pick Index.py
+++ 398. Random Pick Index.py
@@ -1,7 +1,8 @@
 '''
 Problem:
 
-Given an array of integers with possible duplicates, randomly output the index of a given target number. You can assume that the given target number must exist in the array.
+Given an array of integers with possible duplicates, randomly output the index of a given target number. 
+You can assume that the given target number must exist in the array.
 
 Note:
 The array size can be very large. Solution that uses too much extra space will not pass the judge.
