2bd8db51b6320a308e72181aad4702f8b5b74bdc
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-16 16:19:29 -0800

    Update 385. Mini Parser.py

--- 385. Mini Parser.py
+++ 385. Mini Parser.py
@@ -10,15 +10,12 @@ Problem:
     String is non-empty.
     String does not contain white spaces.
     String contains only digits 0-9, [, - ,, ].
-    Example 1:
     
-    Given s = "324",
-    
+    Example 1: Given s = "324",
     You should return a NestedInteger object which contains a single integer 324.
-    Example 2:
     
-    Given s = "[123,[456,[789]]]",
     
+    Example 2: Given s = "[123,[456,[789]]]",
     Return a NestedInteger object containing a nested list with 2 elements:
     
     1. An integer containing value 123.
