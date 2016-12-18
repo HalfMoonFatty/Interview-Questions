d93e11f2241dddd525c4679ba587d8fdbcfe22d0
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-18 11:25:28 -0800

    Update 248. Strobogrammatic Number III.py

--- 248. Strobogrammatic Number III.py
+++ 248. Strobogrammatic Number III.py
@@ -36,13 +36,9 @@ class Solution(object):
                         newres.append(k+item+mp[k])
             return newres
 
+        
 
-        mp = {}
-        mp['0'] = '0'
-        mp['1'] = '1'
-        mp['8'] = '8'
-        mp['6'] = '9'
-        mp['9'] = '6'
+        mp = {'0':'0', '1':'1', '8':'8', '6':'9', '9':'6'}
         count = [0]
 
         # corner case:
