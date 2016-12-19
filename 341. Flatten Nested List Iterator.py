90b8817bf10fd65dbaf51888bbd426053d827f0e
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-19 10:54:30 -0800

    Update 341. Flatten Nested List Iterator.py

--- 341. Flatten Nested List Iterator.py
+++ 341. Flatten Nested List Iterator.py
@@ -16,30 +16,20 @@ By calling next repeatedly until hasNext returns false, the order of elements re
 
 '''
 
+
 # """
 # This is the interface that allows for creating nested lists.
 # You should not implement it, or speculate about its implementation
 # """
 #class NestedInteger(object):
 #    def isInteger(self):
-#        """
-#        @return True if this NestedInteger holds a single integer, rather than a nested list.
-#        :rtype bool
-#        """
 #
 #    def getInteger(self):
-#        """
-#        @return the single integer that this NestedInteger holds, if it holds a single integer
-#        Return None if this NestedInteger holds a nested list
-#        :rtype int
-#        """
 #
 #    def getList(self):
-#        """
-#        @return the nested list that this NestedInteger holds, if it holds a nested list
-#        Return None if this NestedInteger holds a single integer
-#        :rtype List[NestedInteger]
-#        """
+
+
+
 
 class NestedIterator(object):
 
@@ -79,7 +69,3 @@ class NestedIterator(object):
                     self.stack.append([elem.getList(), 0])
         return False
         
-
-# Your NestedIterator object will be instantiated and called as such:
-# i, v = NestedIterator(nestedList), []
-# while i.hasNext(): v.append(i.next())
