9bc85c9ffb5099fb0b0b8a5ecb3b9f021ca500f9
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-19 15:17:12 -0800

    Update 433. Minimum Genetic Mutation.py

--- 433. Minimum Genetic Mutation.py
+++ 433. Minimum Genetic Mutation.py
@@ -3,13 +3,16 @@ Problem:
 
 A gene string can be represented by an 8-character long string, with choices from "A", "C", "G", "T".
 
-Suppose we need to investigate about a mutation (mutation from "start" to "end"), where ONE mutation is defined as ONE single character changed in the gene string.
+Suppose we need to investigate about a mutation (mutation from "start" to "end"), 
+where ONE mutation is defined as ONE single character changed in the gene string.
 
 For example, "AACCGGTT" -> "AACCGGTA" is 1 mutation.
 
-Also, there is a given gene "bank", which records all the valid gene mutations. A gene must be in the bank to make it a valid gene string.
+Also, there is a given gene "bank", which records all the valid gene mutations. 
+A gene must be in the bank to make it a valid gene string.
 
-Now, given 3 things - start, end, bank, your task is to determine what is the minimum number of mutations needed to mutate from "start" to "end". If there is no such a mutation, return -1.
+Now, given 3 things - start, end, bank, your task is to determine what is the minimum number of mutations needed 
+to mutate from "start" to "end". If there is no such a mutation, return -1.
 
 Note:
 Starting point is assumed to be valid, so it might not be included in the bank.
@@ -74,11 +77,9 @@ class Solution(object):
                             
         return -1
                     
-            
-            
-            
 
-
+        
+        
 # Solution 2
 
 
@@ -113,7 +114,3 @@ class Solution(object):
                             q.append(newStr)
                             
         return -1
-                    
-            
-            
-            
