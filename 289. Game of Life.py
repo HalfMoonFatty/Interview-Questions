aece223c9e1ae331d2df44fb13602d7a370dd13b
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-18 17:46:21 -0800

    Update 289. Game of Life.py

--- 289. Game of Life.py
+++ 289. Game of Life.py
@@ -1,9 +1,11 @@
 '''
 Problem:
 
-    According to the Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+    According to the Wikipedia's article: "The Game of Life, also known simply as Life, 
+    is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
 
-    Given a board with m by n cells, each cell has an initial state live (1) or dead (0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+    Given a board with m by n cells, each cell has an initial state live (1) or dead (0). 
+    Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules :
 
     Any live cell with fewer than two live neighbors dies, as if caused by under-population.
     Any live cell with two or three live neighbors lives on to the next generation.
@@ -12,9 +14,11 @@ Problem:
     Write a function to compute the next state (after one update) of the board given its current state.
 
 Follow up:
-    Could you solve it in-place? Remember that the board needs to be updated at the same time: You cannot update some cells first and then use their updated values to update other cells.
-    In this question, we represent the board using a 2D array. In principle, the board is infinite, which would cause problems when the active area encroaches the border of the array. How would you address these problems?
-
+    Could you solve it in-place? Remember that the board needs to be updated at the same time: 
+    You cannot update some cells first and then use their updated values to update other cells.
+    In this question, we represent the board using a 2D array. In principle, the board is infinite, 
+    which would cause problems when the active area encroaches the border of the array. 
+    How would you address these problems?
 '''
 
 '''
