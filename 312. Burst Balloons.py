d0a828e89bc0dfa49537ebf049ee3142d7f17f16
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-18 20:57:11 -0800

    Update 312. Burst Balloons.py

--- 312. Burst Balloons.py
+++ 312. Burst Balloons.py
@@ -1,8 +1,10 @@
 '''
 Problem:
 
-	Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums. You are asked to burst all the balloons. 
-	If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins. Here left and right are adjacent indices of i. 
+	Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums. 
+	You are asked to burst all the balloons. 
+	If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins. 
+	Here left and right are adjacent indices of i. 
 	After the burst, the left and right then becomes adjacent.
 
 	Find the maximum coins you can collect by bursting the balloons wisely.
@@ -18,6 +20,7 @@ Example:
 
 '''
 
+
 '''
 Solution: DP
 
@@ -26,6 +29,7 @@ Solution: DP
 - last- last bursted balloon
 - dp[left][right] -- within in range [left to right] max coins that I can get.
 '''
+
 
 class Solution(object):
     def maxCoins(self, nums):
