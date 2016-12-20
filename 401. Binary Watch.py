e01b6fe1fe9e0014a19c4dd854c4870f9c95a3d6
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-20 09:57:30 -0800

    Update 401. Binary Watch.py

--- 401. Binary Watch.py
+++ 401. Binary Watch.py
@@ -6,6 +6,12 @@ A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6
 Each LED represents a zero or one, with the least significant bit on the right.
 
 
+hours:  8   4   2   1
+        o   o   *   *
+
+mins:   32  16  8   4   2   1
+        o   *   *   o   o   *
+
 For example, the above binary watch reads "3:25".
 
 Given a non-negative integer n which represents the number of LEDs that are currently on, return all possible times the watch could represent.
