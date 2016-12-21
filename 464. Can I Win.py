f1dc0babaa3ba7db2d29f8abb2c6f0a8bb264dd3
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-20 22:58:58 -0800

    Update 464. Can I Win.py

--- 464. Can I Win.py
+++ 464. Can I Win.py
@@ -6,8 +6,7 @@ The player who first causes the running total to reach or exceed 100 wins.
 
 What if we change the game so that players cannot re-use integers?
 
-For example, two players might take turns drawing from a common pool of numbers of 1..15 without replacement 
-until they reach a total >= 100.
+e.g. two players might take turns drawing from a common pool of numbers of 1..15 without replacement until they reach a total >= 100.
 
 Given an integer maxChoosableInteger and another integer desiredTotal, 
 determine if the first player to move can force a win, assuming both players play optimally.
@@ -15,13 +14,8 @@ determine if the first player to move can force a win, assuming both players pla
 You can always assume that maxChoosableInteger will not be larger than 20 and desiredTotal will not be larger than 300.
 
 Example
-
-Input:
-maxChoosableInteger = 10
-desiredTotal = 11
-
-Output:
-false
+Input:   maxChoosableInteger = 10    desiredTotal = 11
+Output:  false
 
 Explanation:
 No matter which integer the first player choose, the first player will lose.
@@ -34,8 +28,7 @@ Same with other integers chosen by the first player, the second player will alwa
 '''
 Solution:
 
-The key part for the top-down dp strategy is that we need to avoid repeatedly solving sub-problems. 
-Instead, we should "remember" the outcome of sub-problems. Then when we see them again, we instantly know their result. 
+The key part for the top-down memorization strategy is that we need to avoid repeatedly solving sub-problems. 
 By applying the memorization, we at most compute for every subproblem once, and there are O(2^n) subproblems, 
 so the complexity is O(2^n) after memorization. (Without memo, time complexity should be like O(n!))
 
@@ -84,12 +77,10 @@ class Solution(object):
             
             
             
-            
         if maxChoosableInteger >= desiredTotal: return True
         if (1+maxChoosableInteger)*maxChoosableInteger/2 < desiredTotal: return False
         
         cache = {}
         used = [False] * (maxChoosableInteger+1)
         return canWin(desiredTotal, used, cache)
-        
         
