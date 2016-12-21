01aa7ece67ee273a1e8cbc953393244f74429122
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-20 23:57:04 -0800

    Update 465. Optimal Account Balancing.py

--- 465. Optimal Account Balancing.py
+++ 465. Optimal Account Balancing.py
@@ -2,9 +2,13 @@
 Problem:
 
 A group of friends went on holiday and sometimes lent each other money. 
+
 For example, Alice paid for Bill's lunch for $10. Then later Chris gave Alice $5 for a taxi ride. 
+
 We can model each transaction as a tuple (x, y, z) which means person x gave person y $z. 
+
 Assuming Alice, Bill, and Chris are person 0, 1, and 2 respectively (0, 1, 2 are the person's ID), 
+
 the transactions can be represented as [[0, 1, 10], [2, 0, 5]].
 
 Given a list of transactions between a group of people, return the minimum number of transactions required to settle the debt.
@@ -36,6 +40,8 @@ Person #2 gave person #0 $5.
 Therefore, person #1 only need to give person #0 $4, and all debt is settled.
 '''
 
+
+
 '''
 Solution: 记忆化搜索
 
@@ -45,6 +51,12 @@ Solution: 记忆化搜索
 
 通过保存中间计算结果可以减少重复搜索 (这道题目似乎是NP Hard).
 '''
+
+
+
+
+
+
 
 class Solution(object):
     def minTransfers(self, transactions):
