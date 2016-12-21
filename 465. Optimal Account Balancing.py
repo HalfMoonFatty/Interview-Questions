aae01a2a5e0a923d518bd2b92ebd459fbd557eed
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-21 10:08:31 -0800

    Update 465. Optimal Account Balancing.py

--- 465. Optimal Account Balancing.py
+++ 465. Optimal Account Balancing.py
@@ -2,14 +2,9 @@
 Problem:
 
 A group of friends went on holiday and sometimes lent each other money. 
-
 For example, Alice paid for Bill's lunch for $10. Then later Chris gave Alice $5 for a taxi ride. 
-
 We can model each transaction as a tuple (x, y, z) which means person x gave person y $z. 
-
-Assuming Alice, Bill, and Chris are person 0, 1, and 2 respectively (0, 1, 2 are the person's ID), 
-
-the transactions can be represented as [[0, 1, 10], [2, 0, 5]].
+Assuming Alice, Bill, and Chris are person 0, 1, and 2 respectively, the transactions can be represented as [[0, 1, 10], [2, 0, 5]].
 
 Given a list of transactions between a group of people, return the minimum number of transactions required to settle the debt.
 
@@ -40,23 +35,13 @@ Person #2 gave person #0 $5.
 Therefore, person #1 only need to give person #0 $4, and all debt is settled.
 '''
 
-
-
 '''
 Solution: 记忆化搜索
 
 统计每个人借出/借入的金钱总数, 将借出金钱的人放入集合rich，借入金钱的人放入集合poor
-
 问题转化为计算从rich到poor的最小“债务连线”数. 尝试用rich中的每个金额与poor中的每个金额做匹配. 若存在差值，则将差值加入相应集合继续搜索
-
-通过保存中间计算结果可以减少重复搜索 (这道题目似乎是NP Hard).
+通过保存中间计算结果可以减少重复搜索 (这道题目似乎是NP Hard). Cache 是 dict of dict, key 必须转化为 tuple.
 '''
-
-
-
-
-
-
 
 class Solution(object):
     def minTransfers(self, transactions):
