626ccbfd26ca7cfb2a0a70c2aa72f883b4b8b656
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-28 22:19:12 -0800

    Update 472. Concatenated Words.py

--- 472. Concatenated Words.py
+++ 472. Concatenated Words.py
@@ -19,6 +19,7 @@ All the input string will only include lower case letters.
 The returned elements order does not matter.
 '''
 
+
 class Solution(object):
     def findAllConcatenatedWordsInADict(self, words):
         """
@@ -42,4 +43,62 @@ class Solution(object):
                 result.append(word)
             wordSet.add(word)
         return result
+       
+
+       
+       
+# Solution 2: 使用字典树Trie替换Set，理论上可以提升查找效率。       
+       
+class Solution(object):
+    def findAllConcatenatedWordsInADict(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        self.trie = Trie()
+        ans = []
+        for word in words:
+            self.trie.insert(word)
+        for word in words:
+            if self.search(word):
+                ans.append(word)
+        return ans
+
+    def search(self, word):
+        node = self.trie.root
+        for idx, letter in enumerate(word):
+            node = node.children.get(letter)
+            if node is None:
+                return False
+            suffix = word[idx+1:]
+            if node.isWord and (self.trie.search(suffix) or self.search(suffix)):
+                return True
+        return False
+
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.isWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for letter in word:
+            child = node.children.get(letter)
+            if child is None:
+                child = TrieNode()
+                node.children[letter] = child
+            node = child
+        node.isWord = True
+
+    def search(self, word):
+        node = self.root
+        for letter in word:
+            node = node.children.get(letter)
+            if node is None:
+                return False
+        return node.isWord
 
