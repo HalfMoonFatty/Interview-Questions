ce857197c4775bc7725480cb3cbc0881f3037f1a
HalfMoonFatty <allofotherstuff@gmail.com>
2016-12-28 19:58:33 -0800

    Create 472. Concatenated Words.py

---
+++ 472. Concatenated Words.py
@@ -0,0 +1,45 @@
+'''
+Problem:
+
+Given a list of words (without duplicates), please write a program that returns all concatenated words in the given list of words.
+
+A concatenated word is defined as a string that is comprised entirely of at least two shorter words in the given array.
+
+Example:
+Input: ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
+Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
+Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats"; 
+ "dogcatsdog" can be concatenated by "dog", "cats" and "dog"; 
+"ratcatdogcat" can be concatenated by "rat", "cat", "dog" and "cat".
+
+Note:
+The number of elements of the given array will not exceed 10,000
+The length sum of elements in the given array will not exceed 600,000.
+All the input string will only include lower case letters.
+The returned elements order does not matter.
+'''
+
+class Solution(object):
+    def findAllConcatenatedWordsInADict(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        def search(word):
+            if word in wordSet:
+                return True
+            for idx in range(1, len(word)):
+                if word[:idx] in wordSet and search(word[idx:]):
+                    return True
+            return False
+        
+        
+        result = []
+        wordSet = set(words)
+        for word in words:
+            wordSet.remove(word)
+            if search(word):
+                result.append(word)
+            wordSet.add(word)
+        return result
+
