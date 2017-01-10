eda180e1d3ec6b5bbfbd0cc5d48003239afb10fe
HalfMoonFatty <allofotherstuff@gmail.com>
2017-01-10 13:19:31 -0800

    Update 391. Perfect Rectangle.py

--- 391. Perfect Rectangle.py
+++ 391. Perfect Rectangle.py
@@ -8,7 +8,6 @@ Each rectangle is represented as a bottom-left point and a top-right point. For 
 
 
 Example 1:
-
 rectangles = [
   [1,1,3,3],
   [3,1,4,2],
@@ -20,7 +19,6 @@ Return true. All 5 rectangles together form an exact cover of a rectangular regi
  
 
 Example 2:
-
 rectangles = [
   [1,1,2,3],
   [1,3,2,4],
@@ -32,7 +30,6 @@ Return false. Because there is a gap between the two rectangular regions.
  
 
 Example 3:
-
 rectangles = [
   [1,1,3,3],
   [3,1,4,2],
@@ -44,7 +41,6 @@ Return false. Because there is a gap in the top center.
  
 
 Example 4:
-
 rectangles = [
   [1,1,3,3],
   [3,1,4,2],
