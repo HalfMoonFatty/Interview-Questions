b577b513c65ab54af22bff1deeaeac3074d53dca
HalfMoonFatty <allofotherstuff@gmail.com>
2017-01-10 13:18:54 -0800

    Update 391. Perfect Rectangle.py

--- 391. Perfect Rectangle.py
+++ 391. Perfect Rectangle.py
@@ -16,10 +16,7 @@ rectangles = [
   [1,3,2,4],
   [2,3,3,4]
 ]
-
 Return true. All 5 rectangles together form an exact cover of a rectangular region.
- 
-
  
 
 Example 2:
@@ -34,8 +31,6 @@ rectangles = [
 Return false. Because there is a gap between the two rectangular regions.
  
 
- 
-
 Example 3:
 
 rectangles = [
@@ -46,8 +41,6 @@ rectangles = [
 ]
 
 Return false. Because there is a gap in the top center.
- 
-
  
 
 Example 4:
