184f5af3a1fb9093b8fb86a0ad8e8d25b0efbbc5
HalfMoonFatty <allofotherstuff@gmail.com>
2017-01-10 23:04:25 -0800

    Update 394. Decode String.py

--- 394. Decode String.py
+++ 394. Decode String.py
@@ -7,7 +7,8 @@ The encoding rule is: k[encoded_string], where the encoded_string inside the squ
 
 You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
 
-Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. 
+For example, there won't be input like 3a or 2[4].
 
 Examples:
 
@@ -16,7 +17,6 @@ s = "3[a2[c]]", return "accaccacc".
 s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
 
 '''
-
 
 
 # Solution 1
@@ -77,5 +77,4 @@ class Solution(object):
         return pattern
         
         
-        
-
+       
