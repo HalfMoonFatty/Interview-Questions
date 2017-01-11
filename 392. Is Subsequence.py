4a2a11d6c8938546df60e1b2d4347eee2e5ca5c1
HalfMoonFatty <allofotherstuff@gmail.com>
2017-01-10 23:02:54 -0800

    Update 392. Is Subsequence.py

--- 392. Is Subsequence.py
+++ 392. Is Subsequence.py
@@ -7,31 +7,23 @@ You may assume that there is only lower case English letters in both s and t.
 t is potentially a very long (length ~= 500,000) string, and s is a short string (<=100).
 
 A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) 
-of the characters without disturbing the relative positions of the remaining characters. 
-(ie, "ace" is a subsequence of "abcde" while "aec" is not).
+of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
 
-Example 1:
-s = "abc", t = "ahbgdc"
+Example 1: s = "abc", t = "ahbgdc"
 Return true.
 
-Example 2:
-s = "axc", t = "ahbgdc"
+Example 2: s = "axc", t = "ahbgdc"
 Return false.
 
 Follow up:
 If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. 
 In this scenario, how would you change your code?
-
 '''
 
 
 class Solution(object):
     def isSubsequence(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+
         q = collections.deque(s)
         for char in t:
             if len(q) and char == q[0]:
@@ -40,9 +32,8 @@ class Solution(object):
             
 
         
-'''
-Use 2 pointers:
-'''
+# Use 2 pointers:
+
 class Solution(object):
     def isSubsequence(self, s, t):
 
@@ -57,18 +48,13 @@ class Solution(object):
         return True
     
     
-'''
-Follow-up:
-https://discuss.leetcode.com/topic/60134/java-code-for-the-problem-two-pointer-and-the-follow-up-binary-search
-'''
+
+
+# Follow-up: https://discuss.leetcode.com/topic/60134/java-code-for-the-problem-two-pointer-and-the-follow-up-binary-search
 
 class Solution(object):
     def isSubsequence(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+
         # binary search to find the first index that is larger than current index
         def getNextIndex(index_list, index):
             if not index_list : return -1
