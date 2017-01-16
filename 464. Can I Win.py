579dcc1a2f40f4c2fecfc006e4aba952946adf05
HalfMoonFatty <allofotherstuff@gmail.com>
2017-01-16 10:44:48 -0800

    Update 464. Can I Win.py

--- 464. Can I Win.py
+++ 464. Can I Win.py
@@ -3,13 +3,10 @@ Problem:
 
 In the "100 game," two players take turns adding, to a running total, any integer from 1..10. 
 The player who first causes the running total to reach or exceed 100 wins.
-
 What if we change the game so that players cannot re-use integers?
-
 e.g. two players might take turns drawing from a common pool of numbers of 1..15 without replacement until they reach a total >= 100.
 
-Given an integer maxChoosableInteger and another integer desiredTotal, 
-determine if the first player to move can force a win, assuming both players play optimally.
+Given an integer maxChoosableInteger and another integer desiredTotal, determine if the first player to move can force a win, assuming both players play optimally.
 
 You can always assume that maxChoosableInteger will not be larger than 20 and desiredTotal will not be larger than 300.
 
@@ -44,11 +41,7 @@ The rest part of the solution is just simulating the game process using the top-
 
 class Solution(object):
     def canIWin(self, maxChoosableInteger, desiredTotal):
-        """
-        :type maxChoosableInteger: int
-        :type desiredTotal: int
-        :rtype: bool
-        """
+
         def convertToState(used):
             state = 0
             for bit in used:
