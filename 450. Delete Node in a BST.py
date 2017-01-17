23580e0770b16da18cb6394e178f202e5480c4c3
HalfMoonFatty <allofotherstuff@gmail.com>
2017-01-17 15:49:59 -0800

    Update 450. Delete Node in a BST.py

--- 450. Delete Node in a BST.py
+++ 450. Delete Node in a BST.py
@@ -1,16 +1,16 @@
 '''
 Problem:
 
-Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. 
+Return the root node reference (possibly updated) of the BST.
 
 Basically, the deletion can be divided into two stages:
 
-Search for a node to remove.
-If the node is found, delete the node.
+Search for a node to remove. If the node is found, delete the node.
+
 Note: Time complexity should be O(height of tree).
 
 Example:
-
 root = [5,3,6,2,4,null,7]
 key = 3
 
@@ -38,7 +38,6 @@ Another valid answer is [5,2,6,null,4,null,7].
    \   \
     4   7
 '''
-
 
 
 class Solution(object):
@@ -69,6 +68,4 @@ class Solution(object):
                 root.val = minVal
                 root.right = self.deleteNode(root.right, minVal)
         return root
-                
-                
                 
