56d7995183af443acb15517ed792ee1f9cfa4d43
HalfMoonFatty <allofotherstuff@gmail.com>
2017-01-18 11:04:42 -0800

    Update 458. Poor Pigs.py

--- 458. Poor Pigs.py
+++ 458. Poor Pigs.py
@@ -19,6 +19,30 @@ We have minutesToTest/minutesToDie "attempts" that are possible in the time span
 death on first attempt, death on second attempt ... death on last attempt, and life. Let the number of possible results be "base".
 
 The problem then becomes finding the smallest n such that pow(base,n)>=buckets, which should be relatively simple.
+
+令r = p / m，表示在规定时间内可以做多少轮“试验”。
+
+假设有3头猪，计算可以确定的最大桶数。
+
+首先考虑只能做1轮试验的情形：
+
+0 1 2 3 4 5 6 7 （桶序号）
+0 0 0 0 1 1 1 1 （第1头猪饮用：4，5，6，7）
+0 0 1 1 0 0 1 1 （第2头猪饮用：2，3，6，7）
+0 1 0 1 0 1 0 1 （第3头猪饮用：1，3，5，7）
+
+可能的试验结果如下（0表示幸存， 1表示死亡）：
+
+0 1 2 （猪序号）
+-----
+0 0 0 （毒药为0号桶，0、1、2号猪均幸存）
+0 0 1 （毒药为1号桶，2号猪死亡）
+0 1 0 （毒药为2号桶，1号猪死亡）
+0 1 1 （毒药为3号桶，1、2号猪死亡）
+1 0 0 （毒药为4号桶，0号猪死亡）
+1 0 1 （毒药为5号桶，0、2号猪死亡）
+1 1 0 （毒药为6号桶，0、1号猪死亡）
+1 1 1 （毒药为7号桶，0、1、2号猪均死亡）
 '''
 
 class Solution(object):
