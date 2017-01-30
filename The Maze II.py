e3a827214dd1f2c6b171a302cc66f48fd829f755
HalfMoonFatty <allofotherstuff@gmail.com>
2017-01-30 15:04:49 -0800

    Update The Maze II.py

--- The Maze II.py
+++ The Maze II.py
@@ -2,11 +2,17 @@
 Problem:
 
 
-There is a ball in a maze with empty spaces and walls. The ball can go through empty spaces by rolling up (u), down (d), left (l) or right (r), but it won't stop rolling until hitting a wall. When the ball stops, it could choose the next direction. There is also a hole in this maze. The ball will drop into the hole if it rolls on to the hole.
+There is a ball in a maze with empty spaces and walls. The ball can go through empty spaces by rolling up (u), down (d), left (l) or right (r), 
+but it won't stop rolling until hitting a wall. When the ball stops, it could choose the next direction. There is also a hole in this maze. 
+The ball will drop into the hole if it rolls on to the hole.
 
-Given the ball position, the hole position and the maze, your job is to find out how the ball could drop into the hole by moving shortest distance in the maze. The distance is defined by the number of empty spaces the ball go through from the start position (exclude) to the hole (include). Output the moving directions by using 'u', 'd', 'l' and 'r'. Since there may have several different shortest ways, you should output the lexicographically smallest way. If the ball cannot reach the hole, output "impossible".
+Given the ball position, the hole position and the maze, find out how the ball could drop into the hole by moving shortest distance in the maze. 
+The distance is defined by the number of empty spaces the ball go through from the start position (exclude) to the hole (include). 
+Output the moving directions by using 'u', 'd', 'l' and 'r'. Since there may have several different shortest ways, 
+you should output the lexicographically smallest way. If the ball cannot reach the hole, output "impossible".
 
-The maze is represented by a binary 2D array. 1 means the wall and 0 means the empty space. You may assume that the borders of the maze are all walls. The ball and hole coordinates are represented by row and column indexes.
+The maze is represented by a binary 2D array. 1 means the wall and 0 means the empty space. You may assume that the borders of the maze are all walls. 
+The ball and hole coordinates are represented by row and column indexes.
 
 Example 1
 
@@ -45,7 +51,6 @@ Explanation: The ball cannot reach the hole.
 
 
 Note:
-
 There are only one ball and one hole in the maze.
 The ball and hole will only exist in the empty space, and they will not at the same position initially.
 The given maze doesn't contain border (like the red rectangle in the example pictures), but you should assume the border of the maze are all walls.
@@ -57,19 +62,19 @@ The maze contains at least 2 empty spaces, and the length and width of the maze 
 '''
 Solution 1：预处理 + 单源最短路
 
-首先预处理迷宫maze中各点坐标向四个方向运动最终可以达到的坐标，用数组dmap记录。
+首先预处理迷宫maze中各点坐标向四个方向运动最终可以达到的坐标，用数组 destmap (destination) 记录。
 
 例如dmap[(3, 2)]['u']表示从坐标(3, 2)出发向上运动最终可以到达的位置。
 
-利用数组bmap记录迷宫中各点坐标到小球初始位置的最短距离dist与最短路径path。
+利用数组ballmap记录迷宫中各点坐标到小球初始位置的最短距离dist与最短路径path。
 
 维护队列queue，初始将(ball, 0, '')加入queue（坐标，距离，路径）
 
-记当前坐标为p，从p出发，分别向u, d, l, r四个方向扩展并更新下一个坐标np的最短距离与路径，保存在bmap中。
+记当前坐标为p，从p出发，分别向u, d, l, r四个方向扩展并更新下一个坐标np的最短距离与路径，保存在ballmap中。
 
 如果np得到更新，则将np加入队列；重复此过程直到queue为空
 
-返回bmap[hole]
+返回ballmap[hole]
 '''
 
 class Solution(object):
@@ -80,34 +85,38 @@ class Solution(object):
         :type hole: List[int]
         :rtype: str
         """
-        ball, hole = tuple(ball), tuple(hole)
-        dmap = collections.defaultdict(lambda: collections.defaultdict(int))
+        def distance(pa, pb):
+            return abs(pa[0] - pb[0]) + abs(pa[1] - pb[1])
+            
         w, h = len(maze), len(maze[0])
-        for dir in 'dlru': dmap[hole][dir] = hole
+        ball, hole = tuple(ball), tuple(hole)
+        # pre-processing destination map
+        destmap = collections.defaultdict(lambda: collections.defaultdict(int))
+        for dir in 'dlru': destmap[hole][dir] = hole
         for x in range(w):
             for y in range(h):
                 if maze[x][y] or (x, y) == hole: continue
-                dmap[(x, y)]['u'] = dmap[(x - 1, y)]['u'] if x > 0 and dmap[(x - 1, y)]['u'] else (x, y)
-                dmap[(x, y)]['l'] = dmap[(x, y - 1)]['l'] if y > 0 and dmap[(x, y - 1)]['l'] else (x, y)
+                destmap[(x, y)]['u'] = destmap[(x - 1, y)]['u'] if x > 0 and destmap[(x - 1, y)]['u'] else (x, y)
+                destmap[(x, y)]['l'] = destmap[(x, y - 1)]['l'] if y > 0 and destmap[(x, y - 1)]['l'] else (x, y)
         for x in range(w - 1, -1, -1):
             for y in range(h - 1, -1, -1):
                 if maze[x][y] or (x, y) == hole: continue
-                dmap[(x, y)]['d'] = dmap[(x + 1, y)]['d'] if x < w - 1 and dmap[(x + 1, y)]['d'] else (x, y)
-                dmap[(x, y)]['r'] = dmap[(x, y + 1)]['r'] if y < h - 1 and dmap[(x, y + 1)]['r'] else (x, y)
-        bmap = {ball : (0, '')}
-        distance = lambda pa, pb: abs(pa[0] - pb[0]) + abs(pa[1] - pb[1])
+                destmap[(x, y)]['d'] = destmap[(x + 1, y)]['d'] if x < w - 1 and destmap[(x + 1, y)]['d'] else (x, y)
+                destmap[(x, y)]['r'] = destmap[(x, y + 1)]['r'] if y < h - 1 and destmap[(x, y + 1)]['r'] else (x, y)
+                
+        ballmap = {ball : (0, '')}
         queue = collections.deque([(ball, 0, '')])
         while queue:
-            front, dist, path = queue.popleft()
+            pos, dist, path = queue.popleft()
             for dir in 'dlru':
-                if dir not in dmap[front]: continue
-                np = dmap[front][dir]
-                ndist = dist + distance(front, np)
+                if dir not in destmap[pos]: continue
+                npos = destmap[pos][dir]
+                ndist = dist + distance(pos, npos)
                 npath = path + dir
-                if np not in bmap or (ndist, npath) < bmap[np]:
-                    bmap[np] = (ndist, npath)
-                    queue.append((np, ndist, npath))
-        return bmap[hole][1] if hole in bmap else 'impossible'
+                if np not in ballmap or (ndist, npath) < ballmap[npos]:
+                    ballmap[npos] = (ndist, npath)
+                    queue.append((npos, ndist, npath))
+        return ballmap[hole][1] if hole in ballmap else 'impossible'
         
         
         
@@ -123,33 +132,37 @@ class Solution(object):
         :type hole: List[int]
         :rtype: str
         """
-        ball, hole = tuple(ball), tuple(hole)
-        dmap = collections.defaultdict(lambda: collections.defaultdict(int))
+        def distance(pa, pb):
+            return abs(pa[0] - pb[0]) + abs(pa[1] - pb[1])
+            
         w, h = len(maze), len(maze[0])
-        for dir in 'dlru': dmap[hole][dir] = hole
+        ball, hole = tuple(ball), tuple(hole)
+        # pre-processing destination map
+        destmap = collections.defaultdict(lambda: collections.defaultdict(int))
+        for dir in 'dlru': destmap[hole][dir] = hole
         for x in range(w):
             for y in range(h):
                 if maze[x][y] or (x, y) == hole: continue
-                dmap[(x, y)]['u'] = dmap[(x - 1, y)]['u'] if x > 0 and dmap[(x - 1, y)]['u'] else (x, y)
-                dmap[(x, y)]['l'] = dmap[(x, y - 1)]['l'] if y > 0 and dmap[(x, y - 1)]['l'] else (x, y)
+                destmap[(x, y)]['u'] = destmap[(x - 1, y)]['u'] if x > 0 and destmap[(x - 1, y)]['u'] else (x, y)
+                destmap[(x, y)]['l'] = destmap[(x, y - 1)]['l'] if y > 0 and destmap[(x, y - 1)]['l'] else (x, y)
         for x in range(w - 1, -1, -1):
             for y in range(h - 1, -1, -1):
                 if maze[x][y] or (x, y) == hole: continue
-                dmap[(x, y)]['d'] = dmap[(x + 1, y)]['d'] if x < w - 1 and dmap[(x + 1, y)]['d'] else (x, y)
-                dmap[(x, y)]['r'] = dmap[(x, y + 1)]['r'] if y < h - 1 and dmap[(x, y + 1)]['r'] else (x, y)
-        bmap = {ball : (0, '', ball)}
-        vset = set()
-        distance = lambda pa, pb: abs(pa[0] - pb[0]) + abs(pa[1] - pb[1])
-        while bmap:
-            dist, path, p = min(bmap.values())
-            if p == hole: return path
-            del bmap[p]
-            vset.add(p)
+                destmap[(x, y)]['d'] = destmap[(x + 1, y)]['d'] if x < w - 1 and destmap[(x + 1, y)]['d'] else (x, y)
+                destmap[(x, y)]['r'] = destmap[(x, y + 1)]['r'] if y < h - 1 and destmap[(x, y + 1)]['r'] else (x, y)
+                
+        ballmap = {ball : (0, '', ball)}
+        visited = set()
+        while ballmap:
+            dist, path, pos = min(ballmap.values())
+            if pos == hole: return path   # find a vlid path
+            del bmap[pos]
+            visited.add(pos)
             for dir in 'dlru':
-                if dir not in dmap[p]: continue
-                np = dmap[p][dir]
-                ndist = dist + distance(p, np)
+                if dir not in dmap[pos]: continue
+                npos = dmap[pos][dir]
+                ndist = dist + distance(pos, npos)
                 npath = path + dir
-                if np not in vset and (np not in bmap or (ndist, npath, np) < bmap[np]):
-                    bmap[np] = (ndist, npath, np)
+                if np not in visited and (npos not in ballmap or (ndist, npath, npos) < bmap[npos]):
+                    bmap[npos] = (ndist, npath, npos)
         return 'impossible'
