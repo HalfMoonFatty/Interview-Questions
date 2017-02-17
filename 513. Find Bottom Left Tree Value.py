1fba13be5b708c9671120576ce3b6a003d73b110
HalfMoonFatty <allofotherstuff@gmail.com>
2017-02-17 11:11:12 -0800

    Create 513. Find Bottom Left Tree Value.py

---
+++ 513. Find Bottom Left Tree Value.py
@@ -0,0 +1,50 @@
+'''
+Problem:
+
+Given a binary tree, find the leftmost value in the last row of the tree.
+
+Example 1:
+Input:
+    2
+   / \
+  1   3
+Output: 1
+
+
+Example 2: 
+Input:
+        1
+       / \
+      2   3
+     /   / \
+    4   5   6
+       /
+      7
+Output: 7
+
+Note: You may assume the tree (i.e., the given root node) is not NULL.
+'''
+
+
+import collections
+class Solution(object):
+    def findBottomLeftValue(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 
+    
+        ans = None
+        q = collections.deque([root])
+        while len(q):
+            size = len(q)
+            ans = q[0].val
+            for i in range(size):
+                cur = q.popleft()
+                if cur.left: q.append(cur.left)
+                if cur.right: q.append(cur.right)
+        return ans
+                
+            
+            
