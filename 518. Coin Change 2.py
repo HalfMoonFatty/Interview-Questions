9735e8f7ef69a6ea7ea7eee33519598f27260afb
HalfMoonFatty <allofotherstuff@gmail.com>
2017-02-27 01:11:00 -0800

    Update 518. Coin Change 2.py

--- 518. Coin Change 2.py
+++ 518. Coin Change 2.py
@@ -44,3 +44,19 @@ class Solution(object):
                 elif j - coins[i] > 0:
                     dp[j] += dp[j-coins[i]]
         return dp[-1]
+
+
+# Concise code:
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for c in coins:
+            for x in range(c, amount + 1):
+                dp[x] += dp[x - c]
+        return dp[amount]
