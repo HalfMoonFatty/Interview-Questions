72954887c33401d6e756c91e5a8cdf3dd67a3d91
HalfMoonFatty <allofotherstuff@gmail.com>
2017-03-08 23:56:08 -0800

    Update 501. Find Mode in Binary Tree.py

--- 501. Find Mode in Binary Tree.py
+++ 501. Find Mode in Binary Tree.py
@@ -40,3 +40,52 @@ class Solution(object):
         traverse(root, count)
         max_freq = max(count.values())
         return [k for k, v in count.iteritems() if v == max_freq]
+
+      
+      
+      
+'''
+Solution 2: In order traversal
+
+Time: O(n)
+Space: O(1)
+'''
+
+class Solution(object):
+    
+    def __init__(self):
+        self.result = []
+        self.prev = None
+        self.count = 1
+        self.maxfreq = 0
+    
+    def findMode(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        def traverse(root):
+            if not root: 
+                return
+            
+            traverse(root.left)
+
+            if self.prev is not None:
+                if root.val == self.prev:
+                    self.count += 1
+                else:
+                    self.count = 1
+
+            if self.count > self.maxfreq:
+                self.maxfreq = self.count 
+                self.result = [root.val]
+            elif self.count == self.maxfreq:
+                self.result.append(root.val)
+
+            self.prev = root.val
+            traverse(root.right)
+            return
+        
+        if not root: return []
+        traverse(root)
+        return self.result
