b610e32cbb45e34901a725adb5a77eefd9fad308
HalfMoonFatty <allofotherstuff@gmail.com>
2017-03-12 23:39:46 -0700

    Update 127. Word Ladder.py

--- 127. Word Ladder.py
+++ 127. Word Ladder.py
@@ -46,6 +46,44 @@ class Solution(object):
         :type wordList: Set[str]
         :rtype: int
         """
+
+        visited = set([beginWord])
+        q = deque()
+        q.append((beginWord,1))
+        neighbours = defaultdict(list)
+        
+        for word in wordList:
+            for i in range(len(word)):
+                token = word[:i] + "*" + word[i+1:]
+                neighbours[token].append(word)
+        
+        while len(q):
+            curr, level = q.popleft()
+            if curr == endWord:
+                return level    # count in the last word
+            for i in range(len(curr)):
+                token = curr[:i] + '*' + curr[i+1:]
+                for nei in neighbours[token]:
+                    if nei not in visited:
+                        visited.add(nei)
+                        q.append((nei,level+1))
+                        
+        return 0
+    
+    
+    
+
+# another piece
+
+from collections import defaultdict, deque
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: Set[str]
+        :rtype: int
+        """
         def wordDiff(wordA, wordB):
             return sum([wordA[x] != wordB[x] for x in range(len(wordA))])
         
