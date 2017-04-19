dd91034d73e1aad92ecd9d13d24392266740e109
HalfMoonFatty <allofotherstuff@gmail.com>
2017-04-18 21:19:28 -0700

    Update 521. Longest Uncommon Subsequence I.py

--- 521. Longest Uncommon Subsequence I.py
+++ 521. Longest Uncommon Subsequence I.py
@@ -31,7 +31,19 @@ Only letters from a ~ z will appear in input strings.
 Solution 1: Brute Force
 
 In the brute force approach we will generate all the possible 2^n subsequences of both the strings and store their number of occurences 
-in a hashmap. Longest subsequence whose frequency is equal to will be the required subsequence. And, if it is not found we will return −1.
+in a hashmap. Longest subsequence whose frequency is equal to 1 will be the required subsequence. And, if it is not found we will return −1.
+
+For example, s = 'abc'
+
+i = 0(000), 1(001), 2(010), 3(011), 4(100), 5(101), 6(110), 7(111)
+j = 0,1,2
+
+e.g. when i = 5(101)
+j = 0 => i = 101 => 'a'
+j = 1 => i = 010 not include
+j = 2 => i = 001 => 'c'
+
+t = 'ac'
 '''
 
 
@@ -45,9 +57,9 @@ class Solution(object):
         """
         strmap = collections.defaultdict(int)
         for s in [a,b]:
-            for i in range(pow(2,len(s))):
+            for i in range(pow(2,len(s))):   # go through 2^n subsequences masks
                 t = ''
-                for j in range(len(s)):
+                for j in range(len(s)):      # for each subsequence mask, right shift len(s) times to check each bit
                     if (i >> j) & 1:
                         t += s[j]
                 strmap[t] += 1
@@ -64,6 +76,13 @@ class Solution(object):
 '''
 Solution 2:
 
+Simple analysis of this problem can lead to an easy solution.
+
+These three cases are possible with string a and b:
+
+a = b If both the strings are identical, it is obvious that no subsequence will be uncommon. Hence, return -1.
+len(a) == len(b) and a!=b: n this case we can consider any string i.e. "abc" or "abd" as a required subsequence, 
+as out of these two strings one string will never be a subsequence of other string. Hence, retur
 
 '''
 
