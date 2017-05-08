821818b1dacb1e4826dc8a1ddb204f5d4b45c6e3
HalfMoonFatty <allofotherstuff@gmail.com>
2017-05-08 12:33:27 -0700

    Update 128. Longest Consecutive Sequence.py

--- 128. Longest Consecutive Sequence.py
+++ 128. Longest Consecutive Sequence.py
@@ -33,9 +33,27 @@ class Solution:
 
 
 '''
-Solution 2: 
-	Time: O(n)
-	Space: O(n)
+Solution 2: Time: O(n) Space: O(n)
+		
+For a new number(for loop), we have four conditions:
+1. check duplicate:
+2. find lower and upper bound
+    1) It will be a new lower end. --> Refresh both upper and lower end
+    2) It will be a new upper end. --> Refresh both upper and lower end
+    3) Neither: It is both upper and lower end by itself
+           --> Add the number to the keyset with the value as itself.
+    4) Both: It connects two existing sequence.Its own value is not important
+           --> Add the number to the keyset with the value as itself.
+           --> Refresh both upper and lower end.
+
+
+3. update maxLen
+     maxLen = max(maxLen, the old left boundary n+1 plus the old length m[n+1] - 1)
+
+4. update dict entry of low and high entry value
+    mp[i] = i
+    mp[low] = high
+    mp[high] = low
 '''
 
 class Solution(object):
