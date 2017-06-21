b38658934955948b96b0ad2b5519bde6082972d7
HalfMoonFatty <allofotherstuff@gmail.com>
2017-06-21 13:03:46 -0700

    Update 158. Read N Characters Given Read4 II - Call multiple times.py

--- 158. Read N Characters Given Read4 II - Call multiple times.py
+++ 158. Read N Characters Given Read4 II - Call multiple times.py
@@ -10,6 +10,58 @@ Note:
     
 '''
 
+
+
+
+# Follow-Up: The read function may be called multiple times.
+
+# record what previously has been read
+
+class Solution(object):
+    def __init__(self):
+        self.buff = [None]*4096
+        self.buffPtr = 0
+        self.count = 0
+        self.hasRead = 0
+        self.EOF = False
+
+
+    def read(self, buf, n):
+        """
+            :type buf: Destination buffer (List[str])
+            :type n: Maximum number of characters to read (int)
+            :rtype: The number of characters read (int)
+            """
+            
+        
+        while self.count < n and not EOF:
+         
+            # no more data in the internal buffer, need to read in new data
+            if self.buffPtr == 0:
+                self.hasRead = read4K(self.buff)
+
+            # have no more to read-in
+            if self.hasRead < 4096:
+                EOF = True
+
+            # 搬砖ing, 一次搬4块
+            while self.count < n and self.buffPtr < self.hasRead:    # note loop condition
+                buf[self.count] = self.buff[self.buffPtr]
+                self.buffPtr += 1
+                self.count += 1
+
+            # drained up the internal buffer, reset the buffPtr to 0
+            if self.buffPtr == self.hasRead:
+                self.buffPtr = 0
+
+        return self.count
+
+    
+    
+    
+
+
+
 '''
 Solution:
 
