4786b5920dc56857fc07e3ed93aad051df24ab48
HalfMoonFatty <allofotherstuff@gmail.com>
2017-06-26 22:28:30 -0700

    Update 211. Add and Search Word - Data structure design.py

--- 211. Add and Search Word - Data structure design.py
+++ 211. Add and Search Word - Data structure design.py
@@ -76,3 +76,69 @@ class WordDictionary(object):
 
         return node.isWord
         
+
+# support "*"
+class TrieNode():
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+        
+
+class WordDictionary(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+
+    def addWord(self, word):
+        node = self.root
+        for letter in word:
+            if not node.children.has_key(letter):
+                node.children[letter] = TrieNode()
+            node = node.children[letter]
+        node.isWord = True
+        return
+
+
+    def search(self, word):
+        return self.searchHelper(word, self.root)
+
+
+    def searchHelper(self, word, node):
+        for i in range(len(word)):
+        
+            # regular char: exact matching
+            if len(node.children)>0 and word[i] != '.' and word[i] != '*':
+                if not node.children.has_key(word[i]):
+                    return False
+                node = node.children[word[i]]
+                
+            # wild char"."
+            elif len(node.children)>0 and word[i] == '.':
+                tmp = node    # must remember the current iterator node, as other recursion path may modify node
+                for k in node.children.keys():
+                    node = tmp.children[k] # re-assign value to node
+                    if self.searchHelper(word[i+1:], node):
+                        return True
+
+            elif len(node.children)>0 and word[i] == '*':
+                if self.searchHelper(word[i+1:], node): 
+                    return True
+                tmp = node    # must remember the current iterator node, as other recursion path may modify node
+                for k in node.children.keys():
+                    node = tmp.children[k] # re-assign value to node
+                    if self.searchHelper(word[i:], node):
+                        return True
+
+                        
+            # still have more letters to search (as we are in the loop), but no more children to search.
+            else:
+                return False
+
+        return node.isWord
+
+
+wd = WordDictionary()
+
+wd.addWord("badddd")
+print wd.search("bade*") 
+
