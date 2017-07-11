01162d3e9a5e9260738381d01735b76ff2af5656
HalfMoonFatty <allofotherstuff@gmail.com>
2017-07-10 21:01:44 -0700

    Update 583. Delete Operation for Two Strings.py

--- 583. Delete Operation for Two Strings.py
+++ 583. Delete Operation for Two Strings.py
@@ -47,7 +47,7 @@ class Solution(object):
                 if word1[i-1] == word2[j-1]:
                     dp[i][j] = dp[i-1][j-1] # no need to delete
                 else:
-                    dp[i][j] = min(dp[i-1][j], dp[i][j-1])+1
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1 # delete a char from word1 or from word2
         return dp[-1][-1]
         
         
@@ -70,14 +70,16 @@ class Solution(object):
         def lcs(word1, word2):
             m,n = len(word1),len(word2)
             dp = [[0] * (n+1) for _ in range(m+1)]
-            
+
             for i in range(1,m+1):
                 for j in range(1,n+1):
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
                     if word1[i-1] == word2[j-1]:
                         dp[i][j] = dp[i-1][j-1] + 1
+                    else:
+                        dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
             return dp[-1][-1]
         
         return len(word1)+len(word2)-2*lcs(word1, word2)
-        
+                
                 
