9f0f431781fc0d5c9df3e46193b759ca24f36c98
HalfMoonFatty <allofotherstuff@gmail.com>
2017-07-17 17:16:57 -0700

    Update 616. Add Bold Tag in String.py

--- 616. Add Bold Tag in String.py
+++ 616. Add Bold Tag in String.py
@@ -32,19 +32,19 @@ Solution: 朴素解法
 
 变量start， end记录当前需要加粗标记的子串起止下标，初始为-1
 
-在s中枚举子串起点x，记当前字符为c
+在s中枚举子串起点i，记当前字符为c
 
-遍历dict，寻找与s[x:]匹配长度最长的子串，记其长度为nend
+遍历dict，寻找与s[i:]匹配长度最长的子串，记其长度为nend
 
     若nend > 0：
     
-        若start为-1，将其设为x
+        若start为-1，将其设为i
         
-        将end更新为其与nend + x的较大值
+        将end更新为其与nend + i的较大值
     
     否则：
     
-         若 x >= end > 0：将<b> + s[start:end] + </b>累加至答案，并将start与end重置为-1
+         若 i >= end > 0：将<b> + s[start:end] + </b>累加至答案，并将start与end重置为-1
          
          若start == -1：将c累加至答案
 
@@ -62,16 +62,16 @@ class Solution(object):
         """
         start = end = -1
         ans = ''
-        for x, c in enumerate(s):
+        for i, c in enumerate(s):
             nend = 0
-            for d in dict:
-                if s[x:].startswith(d):
-                    nend = max(nend, len(d))
+            for word in dict:
+                if s[i:].startswith(word):
+                    nend = max(nend, len(word))
             if nend:
-                if start == -1: start = x
-                end = max(end, nend + x)
+                if start == -1: start = i
+                end = max(end, nend + i)
                 continue
-            if x >= end:
+            if i >= end:
                 ans += '<b>' + s[start:end] + '</b>'
                 start = end = -1
             if start == -1: ans += c
