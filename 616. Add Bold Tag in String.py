80f1504ff1d684ea51418e09484cd71d53feb9ed
HalfMoonFatty <allofotherstuff@gmail.com>
2017-07-21 15:58:08 -0700

    Update 616. Add Bold Tag in String.py

--- 616. Add Bold Tag in String.py
+++ 616. Add Bold Tag in String.py
@@ -26,6 +26,58 @@ All the strings in input have length in range [1, 1000].
 '''
 
 '''
+Solution:
+
+Time complexity : O(l∗s∗x). Three nested loops are there to fill bold array.
+Space complexity : O(s). res and bold size grows upto O(s).
+
+nother idea could be to merge the process of identification of the substrings in s matching with the words in dict. 
+To do so, we make use of an array bold for marking the positions of the substrings in s which are present in dict. 
+A True value at bold[i] indicates that the current character is a part of the substring which is present in dict.
+
+We identify the substrings in s by considering only substrings of "lengthd" for a dictionary word d. 
+Whenver such a substring is found with its beginning index as i(and end index (i+lengthd−1)), we mark all such positions in bold as True.
+
+Thus, in this way, whenever a overlapping or consecutive matching substrings exist in s, a continuous sequence of True values is present in bold. 
+Keeping this idea in mind, we traverse over the string s and keep on putting the current character in the resultant string res. 
+At every step, we also check if the bold array contains the beginning or end of a continuous sequence of True values. 
+At the beginnning of such a sequence, we put an opening bold tag and then keep on putting the characters of s till we find a position 
+corresponding to which the last sequence of continuous True values breaks(the first False value is found). We put a closing bold tag at such a position. 
+After this, we again keep on putting the characters of s in res till we find the next True value and we keep on continuing the process in the same manner.
+'''
+
+class Solution(object):
+    def addBoldTag(self, s, dict):
+        """
+        :type s: str
+        :type dict: List[str]
+        :rtype: str
+        """
+        # fill bold
+        bold = [False] * len(s)
+        for word in dict:
+            for i in range(len(s)-len(word)+1):
+                if s[i:i+len(word)] == word:
+                        bold[i:i+len(word)] = [True] * len(word)
+
+        # generate res according to bold
+        res = ''
+        i = 0
+        while i < len(s):
+            if bold[i]:
+                res += '<b>'
+                while i < len(s) and bold[i]:
+                    res += s[i]
+                    i += 1
+                res += '</b>'
+            else:
+                res += s[i]
+                i += 1
+
+        return res
+
+
+'''
 Solution: 朴素解法
 
 时间复杂度O(n * m)，其中n为s的长度，m为dict中字符串长度之和
