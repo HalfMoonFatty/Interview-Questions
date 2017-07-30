f46b2e5dbf4ad4731c96af08bce42403b4e4a4f1
HalfMoonFatty <allofotherstuff@gmail.com>
2017-07-29 22:23:07 -0700

    Update 483. Smallest Good Base.py

--- 483. Smallest Good Base.py
+++ 483. Smallest Good Base.py
@@ -26,11 +26,44 @@ The string representing n is always valid and will not have leading zeros.
 '''
 
 
+'''
+Solution 1: 枚举法
+
+the smallest base is 2 so we can find our m must be between log2n (base = 2) and 2(base = n-1)
+
+记k的最高次幂为m，从上界 int(log(n)) 向下界 1 递减枚举m
+
+问题转化为计算1 + k + k^2 + ... + k^m = n的正整数解
+
+由n > k^m得： k < n ** 1/m
+
+由n < (k + 1)^m得： k > n ** 1/m - 1，此处使用了二项式定理
+
+因此k可能的解为：int(n ** 1/m)
+
+最后验证1 + k + k^2 + ... + k^m 是否等于 n
+
+Time: O(logn)
+'''
+
+class Solution(object):
+    def smallestGoodBase(self, n):
+        """
+        :type n: str
+        :rtype: str
+        """
+        n = int(n)
+        for m in range(int(math.log(n, 2)), 1, -1):
+            k = int(n ** (1.0 / m))
+            if sum(k ** i for i in range(m + 1)) == n:
+                return str(k)
+        return str(n - 1)
+
 
 
 
 '''
-Solution 1: Binary Search
+Solution 2: Binary Search
 
 
 题目要求找到最小的 k, 满足 1+k^1+k^2+k^3+...+k^d=n. 
@@ -54,41 +87,42 @@ The left limit is 1, and the right limit is pow(n,1/d)+1, i.e., the d th square 
 因为我们是从最长的length往下猜，那么对于一个length 如果最小的base 2 都比“n”大，那么更大的base就会更大了。
 
 说明这个长度太长了，就不用再试base了，直接缩短length再猜。
-'''
-
-
-
-
-
-
 
 '''
-Solution 2: 枚举法
 
-the smallest base is 2 so we can find our m must be between log2n (base = 2) and 2(base = n-1)
 
-记k的最高次幂为m，从上界 int(log(n)) 向下界 1 递减枚举m
 
-问题转化为计算1 + k + k^2 + ... + k^m = n的正整数解
+class Solution {
+public:
+    string smallestGoodBase(string n) {
+    unsigned long long tn=(unsigned long long)stoll(n);
+    unsigned long long x=1;
+    for (int i=62;i>=1;i--) {
+        if ((x<<i)<tn) {
+            unsigned long long cur=mysolve(tn,i);
+            if (cur!=0) return to_string(cur);
+        }
+    }
+    return to_string(tn-1);
+    }
+    
+unsigned long long mysolve(unsigned long long n,int d) {
+    double tn=(double) n;
+    unsigned long long right=(unsigned long long)(pow(tn,1.0/d)+1);
+    unsigned long long left=1;
+    while (left<=right){
+        unsigned long long mid=left+(right-left)/2;
+        unsigned long long sum=1,cur=1;
+        for (int i=1;i<=d;i++) {
+            cur*=mid;
+            sum+=cur;
+        }
+        if (sum==n) return mid;
+        if (sum>n) right=mid-1;
+        else left=mid+1;
+    }
+    return 0;
+}
 
-由n > k^m得： k < n ** 1/m
+};
 
-由n < (k + 1)^m得： k > n ** 1/m - 1，此处使用了二项式定理
-
-因此k可能的解为：int(n ** 1/m)
-
-最后验证1 + k + k^2 + ... + k^m 是否等于 n
-'''
-
-class Solution(object):
-    def smallestGoodBase(self, n):
-        """
-        :type n: str
-        :rtype: str
-        """
-        n = int(n)
-        for m in range(int(math.log(n, 2)), 1, -1):
-            k = int(n ** (1.0 / m))
-            if sum(k ** i for i in range(m + 1)) == n:
-                return str(k)
-        return str(n - 1)
