603b585fe35663f78c8f3f36f2d64dc69bd82384
HalfMoonFatty <allofotherstuff@gmail.com>
2017-08-02 21:51:49 -0700

    Update 471. Encode String with Shortest Length.py

--- 471. Encode String with Shortest Length.py
+++ 471. Encode String with Shortest Length.py
@@ -41,46 +41,46 @@ Output: "2[2[abbb]c]"
 Explanation: "abbbabbbc" occurs twice, but "abbbabbbc" can also be encoded to "2[abbb]c", so one answer can be "2[2[abbb]c]".
 '''
 
+
 class Solution(object):
+  def encode (self, s):
+    """Encodes the given string with optimal encoding.
+    Args:
+      s: string to encode.
+    """
+    # dp[i][j] = optimal encoding for substring s[i:j+1]
+    dp = [[''] * len(s) for _  in range(len(s))]
 
-    def encode(self, s):
+    for l in range(len(s)):
+      #Each outer loop will compute optimal encoding for all substring of length l.
+      for i in range(len(s) - l):
+        j = i + l
+        substr = s[i:j + 1]
+        dp[i][j] = substr
+        # Skip if substr length < 5. In that case, we know that encoding will
+        # not help.
+        if l >= 4:
+          # Loop for trying all results that we get after dividing the substr into 2
+          # pieces, and combine the results for the pieces.
+          for k in range(i, j):
+            # k is the split point of the 2 pieces of the substring. We only need to
+            # cut *once*, because dp[i][k] is already the optimal encoding for
+            # substr[i:k+1], which in itself may include multiple x[yzk] encodings.
+            if len(dp[i][k] + dp[k + 1][j]) < len(dp[i][j]):
+              # Split at k is better. Update existing solution.
+              dp[i][j] = dp[i][k] + dp[k + 1][j]
 
-        def isEncoded(s):
-            if not s: return False
-            index = s.find('[')
-            if index == -1: return False
-            val = s[:index]
-            if not val.isdigit(): return False
-            if s[-1] != ']': return False
-            return True
-
-
-        length = len(s)
-        if length <= 1: return s
-        res = [[None]*length for _ in range (length)]
-        dp = [[0]*length for _ in range (length)]
-
-        for l in range(length):
-            for i in range(length - l):
-                if s[i:i+l+1] == s[i] * length: 
-                    res[i][i+l] = str(length) + '[' + s[i] + ']'
-                    dp[i][i+l] = len(res[i][i+l])
-                else:
-                    res[i][i+l] = s[i:i+l+1]
-                    dp[i][i+l] = l+1
-
-                for j in range(i, i + l):
-                    string = res[i][j] + res[j+1][i+l]
-                    minLength = dp[i][j] + dp[j+1][i+l]
-                    string1 = res[i][j][res[i][j].find('[') + 1 : -2] if isEncoded(res[i][j]) else res[i][j]
-                    count1 = int(res[i][j][:res[i][j].find('[')]) if isEncoded(res[i][j]) else 1
-                    string2 = res[j + 1][i + l][res[j + 1][i + l].find('[') + 1 : -2] if isEncoded(res[j+1][i+l]) else res[j+1][i+l]
-                    count2 = int(res[j + 1][i + l][:res[j + 1][i + l].find('[')]) if isEncoded(res[j+1][i+l]) else 1
-
-                    if string1 == string2:
-                        string = str(count1 + count2) + '[' + string1 + ']'
-                        minLength = len(string)
-                    if minLength < dp[i][i+l]: 
-                        res[i][i+l] = string 
-                        dp[i][i+l] = minLength
-        return res[0][-1]
+          # Loop for checking if the substring can itself found some pattern in
+          # it which could be repeated. This is the base case for initiating the
+          # encoding. We only check for single-pattern encodings, i.e. encodings
+          # where there is exactly one pair of []. This is because we start l from
+          # low to high, so if the optimal solution has multiple pairs of [], it
+          # will be covered in the previous if block (L23-L25).
+          for k in range(len(substr)):
+            repeat_str = substr[0:k + 1]
+            multiple = len(substr) / len(repeat_str)
+            if substr == repeat_str * multiple:
+              ss = '{0}[{1}]'.format(multiple, dp[i][i+k])
+              if len(ss) < len(dp[i][j]):
+                dp[i][j] = ss
+    return dp[0][len(s) - 1]
