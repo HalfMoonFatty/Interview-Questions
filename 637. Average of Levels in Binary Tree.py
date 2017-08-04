c8fc4f7e33fc520e20e35a5376ef3ce65cd8dce9
HalfMoonFatty <allofotherstuff@gmail.com>
2017-08-04 14:38:35 -0700

    Update 637. Average of Levels in Binary Tree.py

--- 637. Average of Levels in Binary Tree.py
+++ 637. Average of Levels in Binary Tree.py
@@ -22,6 +22,14 @@ The range of node's value is in the range of 32-bit signed integer.
 '''
 
 
+
+'''
+Time complexity : O(n). The whole tree is traversed atmost once. Here, n refers to the number of nodes in the given binary tree.
+
+Space complexity : O(m). The size of queue or temp can grow upto atmost the maximum number of nodes at any level in the given binary tree. 
+Here, m refers to the maximum mumber of nodes at any level in the input tree.
+'''
+
 class Solution(object):
     def averageOfLevels(self, root):
         """
