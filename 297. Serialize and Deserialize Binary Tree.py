d89754fcac0b84f755b39ad7bd05b66c2946c1ef
HalfMoonFatty <allofotherstuff@gmail.com>
2017-08-06 14:06:09 -0700

    Update 297. Serialize and Deserialize Binary Tree.py

--- 297. Serialize and Deserialize Binary Tree.py
+++ 297. Serialize and Deserialize Binary Tree.py
@@ -23,6 +23,82 @@ Note: Do not use class member/global/static variables to store states. Your seri
 
 
 
+
+# Solution 1: Preorder Traversal
+
+
+class Codec:
+    def serialize(self, root):
+
+        def serHelper(root, output):
+            if not root:
+                output.append('#')
+            else:
+                output.append(str(root.val))
+                serHelper(root.left, output)
+                serHelper(root.right, output)
+       
+        output = []     
+        serHelper(root, output)
+        return ' '.join(output)
+   
+   
+    def deserialize(self, data):
+
+        def desHelper(input):
+            if self.index > len(input) or input[self.index] == '#':
+                return None
+            else:
+                root = TreeNode(input[self.index])
+                self.index += 1
+                root.left = desHelper(input)
+                self.index += 1
+                root.right = desHelper(input)
+                return root
+       
+        self.index = 0
+        input = data.split()
+        return desHelper(input)
+    
+    
+
+    
+# more concise code    
+class Codec:
+
+    def serialize(self, root):
+        def helper(node):
+            if node:
+                vals.append(str(node.val))
+                helper(node.left)
+                helper(node.right)
+            else:
+                vals.append('#')
+        vals = []
+        helper(root)
+        return ' '.join(vals)
+
+    
+    
+    def deserialize(self, data):
+        
+        def helper():
+            val = next(vals)
+            if val == '#':
+                return None
+            node = TreeNode(int(val))
+            node.left = helper()
+            node.right = helper()
+            return node
+        
+        vals = iter(data.split())
+        return helper()
+    
+    
+    
+
+# Solution 2: level order traversal
+
 class Codec:
 
     def serialize(self, root):
@@ -73,68 +149,3 @@ class Codec:
 
 
     
-
-'''
-Solution 2: Preorder Traversal
-
-    - Serialize: 一个比较简单直接的做法是，通过前序遍历preorder traversal来做，把所有空节点当做“#”来标示。注意一下3点：
-        1. 需要用‘,’ 来做delimiter;
-        如果不加delimiter 会怎样呢？ 例如：[-1,0,1]
-        不加任何delimiter serialize 后就是：-10##1## (-,1,0,#,#,1,#,#); “-“ 作为一个char被分出来成了“Node”
-        但是“—” 不应单独作为一个Node，serialize的结果应该是: -1,0,#,#,1,#,#
-        
-        2. 由于每个node后都加一个 delimiter, 所以最后一个node后面也有一个delimiter.
-        如果不去掉最后一个delimiter，在deserialize时，convert string to list最后会多一个 ‘’ element: ['-1', ‘0', '#', '#', '1', '#', ‘#’，‘’]
-        所以在serialize的时候就要把最后的“,” 去掉: return ret[:-1]
-       
-        3. 由于不能用 class member/global/static variables，所以只能用list (obj) go through recursion call.
-        因为function return type是string, 所以最后还要 list convert to string: ret = ''.join(output)
-
-    - Deserialize:
-        1. convert string to list: input = data.split(',’)
-        2. 由于不能用 class member/global/static variables，所以只能用index (list with one element) go through recursion call.
-        
-        main idea:
-        We read elements in the input list one at a time using pre-order traversal.
-        - If the token is a sentinel, meaning it is a None node, thus return None
-        - If the token is a number, we insert it to the current node,
-        - and traverse to its left child with next element in the list (index += 1)
-        - then its right child with next element (index += 1)
-'''
-
-
-class Codec:
-    def serialize(self, root):
-
-        def serHelper(root, output):
-            if not root:
-                output.append('#')
-                output.append(',')
-            else:
-                output.append(str(root.val))
-                output.append(',')
-                serHelper(root.left, output)
-                serHelper(root.right, output)
-       
-        output = []     
-        serHelper(root, output)
-        ret = ''.join(output)
-        return ret[:-1]    # remove the last ""
-   
-   
-    def deserialize(self, data):
-
-        def desHelper(input, index):
-            if index[0] > len(input) or input[index[0]] == '#':
-                return None
-            else:
-                root = TreeNode(input[index[0]])
-                index[0] += 1
-                root.left = desHelper(input,index)
-                index[0] += 1
-                root.right = desHelper(input,index)
-                return root
-       
-        input = data.split(',')
-        index = [0]
-        return desHelper(input, index)
