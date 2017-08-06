5f1f9673e6c5a68299499824876527ff07b42d9d
HalfMoonFatty <allofotherstuff@gmail.com>
2017-08-05 17:53:25 -0700

    Update 404. Sum of Left Leaves.py

--- 404. Sum of Left Leaves.py
+++ 404. Sum of Left Leaves.py
@@ -15,7 +15,56 @@ There are two left leaves in the binary tree, with values 9 and 15 respectively.
 '''
 
 
+# recursive:
 
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0
+        ans = 0
+        if root.left:
+            if not root.left.left and not root.left.right:
+                ans += root.left.val    # left leaf node
+            else:
+                ans += self.sumOfLeftLeaves(root.left)
+        ans += self.sumOfLeftLeaves(root.right)
+        return ans
+    
+    
+    
+# iterative:
+
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0
+        
+        ans = 0
+        stack = [root]
+        while len(stack):
+            node = stack.pop()
+            if node.left:
+                if not node.left.left and not node.left.right:
+                    ans += node.left.val
+                else:
+                    stack.append(node.left)
+            if node.right:
+                if node.right.left or node.right.right:   # node.right is not leaf node
+                    stack.append(node.right)
+        return ans
+    
+
+    
+    
+    
+    
+    
 class Solution(object):
     def sumOfLeftLeaves(self, root):
 
