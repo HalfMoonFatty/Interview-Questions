579447e0c1662e555f8402d9c2d58ac1afc8e9e6
HalfMoonFatty <allofotherstuff@gmail.com>
2017-08-09 15:42:19 -0700

    Update 161. One Edit Distance.py

--- 161. One Edit Distance.py
+++ 161. One Edit Distance.py
@@ -9,39 +9,39 @@ Problem:
 
 class Solution(object):
     def isOneEditDistance(self, s, t):
-
-        def isOneModify(s,t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        def isOneModified(s,t):
             count = 0
             for i in range(len(s)):
-                if s[i] != t[i]: count += 1
+                if s[i] != t[i]:
+                    count += 1
             return count == 1
         
         
-        def isOneDel(s,t):
+        def isOneDelete(s,t):
             if len(t) < len(s):
                 s,t = t,s
-            i,j = 0,0
-            while i<len(s) and j<len(t):
-                if s[i] != t[j]: 
+            i = j = 0
+            while i < len(s) and j < len(t):
+                if s[i] != t[j]:
                     break
                 i += 1
                 j += 1
             return s[i:] == t[j+1:]
+
         
+        if abs(len(s) - len(t)) > 1: 
+            return False
+        if len(s) == len(t): 
+            return isOneModified(s,t)
+        else:
+            return isOneDelete(s,t)
         
-        if len(t) - len(s) > 1: return False
-        if len(s) == len(t): return isOneModify(s,t)
-        else: return isOneDel(s,t)
 
             
             
-        
-        
-        
-        
-
-            
-            
-            
-        
-        
+    
