4c73d523d080aa02cae1c3ae3e936ec0c1ac87af
HalfMoonFatty <allofotherstuff@gmail.com>
2017-08-17 23:37:48 -0700

    Update 247. Strobogrammatic Number II.py

--- 247. Strobogrammatic Number II.py
+++ 247. Strobogrammatic Number II.py
@@ -12,6 +12,7 @@ Hint: Try to use recursion and notice that it should recurse with n - 2 instead 
 
 # Solution 1:
 
+
 class Solution(object):
     def findStrobogrammatic(self, n):
 
@@ -23,17 +24,14 @@ class Solution(object):
             result = getStrob(k-2)
             newresult = []
             for item in result:
-                if k == n:
-                    for x in mp.keys():
-                        if x == '0': continue  # avoid leading '0'
-                        newresult.append(x+str(item)+mp[x])
-                else:
-                    for x in mp.keys():
-                        newresult.append(x+str(item)+mp[x])
+                for x in mp.keys():
+                    if k == n and x == '0': continue   # 最外层不能 0 开头
+                    newresult.append(x+str(item)+mp[x])
             return newresult
 
 
         mp = {'0':'0', '1':'1', '8':'8', '6':'9', '9':'6'}
         return getStrob(n)
+        
         
         
