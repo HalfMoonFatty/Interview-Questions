117c3169505580725535c62b4d5a6ef96b193a28
HalfMoonFatty <allofotherstuff@gmail.com>
2017-09-05 15:48:42 -0700

    Update 028. Implement strStr().py

--- 028. Implement strStr().py
+++ 028. Implement strStr().py
@@ -22,11 +22,9 @@ class Solution(object):
         
         for i in range(m-n+1):
             j = 0
-            while j < n:
-                if haystack[i+j] != needle[j]:
-                    break
-                j +=1
-
+            while j < n and haystack[i + j] == needle[j]:   
+                j += 1
+                
             if j == n:
                 return i
         return -1
