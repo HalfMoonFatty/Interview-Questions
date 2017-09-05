a685d0a9a5da94845cafba6ca7305bf3108d1969
HalfMoonFatty <allofotherstuff@gmail.com>
2017-09-04 17:12:57 -0700

    Update 653. Two Sum IV - Input is a BST.py

--- 653. Two Sum IV - Input is a BST.py
+++ 653. Two Sum IV - Input is a BST.py
@@ -45,3 +45,23 @@ class Solution(object):
         
         valSet = set()
         return canFind(root, k, valSet)
+
+
+'''    
+Solution 2:
+
+The idea is to use a sorted array to save the values of the nodes in the BST by using an inorder traversal. 
+
+Then, we use two pointers which begins from the start and end of the array to find if there is a sum k.
+
+Time Complexity: O(n), Space Complexity: O(n).
+'''
+
+
+'''
+Solution 3:
+
+The idea is to use binary search method. For each node, we check if k - node.val exists in this BST.
+
+Time Complexity: O(nlogn), Space Complexity: O(h). h is the height of the tree, which is logn at best case, and n at worst case.
+'''
