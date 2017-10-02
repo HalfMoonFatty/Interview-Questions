03386091b246e6d3a411c3c85cd569087d600b8d
HalfMoonFatty <allofotherstuff@gmail.com>
2017-10-01 20:45:13 -0700

    Update 098. Validate Binary Search Tree.py

--- 098. Validate Binary Search Tree.py
+++ 098. Validate Binary Search Tree.py
@@ -9,6 +9,9 @@ Assume a BST is defined as follows:
     Both the left and right subtrees must also be binary search trees.
 '''
 
+
+# Recursive
+
 import sys
 class Solution(object):
     def isValidBST(self, root):
@@ -24,3 +27,27 @@ class Solution(object):
             return validBST(root.left, minVal, root.val) and validBST(root.right, root.val, maxVal)
 
         return validBST(root, -sys.maxint+1, sys.maxint)
+
+    
+# Iterative:
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root: return True
+        stack = []
+        prev = None
+        
+        while root or len(stack):
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if prev and root.val <= prev.val:
+                return False
+            prev = root
+            root = root.right
+        return True
